99383e2720ed051cf2d405ed9750eb6a3b0e3761
Dru Jensen <drujensen@gmail.com>
2016-12-30 16:11:06 -0800

    add resource templates

--- src/kemalyst-generator/commands/generate.cr
+++ src/kemalyst-generator/commands/generate.cr
@@ -1,18 +1,19 @@
-require "crustache"
+require "../generator"
 
 module Kemalyst::Generator
 
   class MainCommand < Cli::Supercommand
     command "g", aliased: "generate"
-    
+
     class Generate < Cli::Command
       class Options
-        arg "type", desc: "resource type: model, view, controller", required: true
-        arg "name", desc: "Name of resource", required: true
+        arg "type", desc: "resource", required: true
+        arg "name", desc: "name of resource", required: true
       end
 
       def run
-        puts "Todo: #{args.type} #{args.name}"
+        generator = ::Generator.new(args.name, ".")
+        generator.generate args.type
       end
     end
   end
