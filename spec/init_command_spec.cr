681746fd4940e01799700b3e24daf9fa81d3dc95
Arnaud Fernandés <arnaud.fernandes@tech-magister.com>
2017-01-02 09:39:02 +0100

    Improve init command spec

--- spec/init_command_spec.cr
+++ spec/init_command_spec.cr
@@ -5,8 +5,18 @@ describe Kemalyst::Generator do
   context "Init command" do
 
     it "should create the new project" do
-      Kemalyst::Generator::MainCommand.run %w(init app ../testing/testapp)
-      Dir.exists?("../testing/testapp").should be_true
+      Kemalyst::Generator::MainCommand.run ["init", "app", TESTING_APP]
+      Dir.exists?(TESTING_APP).should be_true
+    end
+
+    it "should create the structure" do
+      dirs = Dir.glob("#{APP_TPL_PATH}/**/*").select {|e| Dir.exists? e }
+      rel_dirs = dirs.map { |dir| dir[(APP_TPL_PATH.size+1)..-1] }
+
+      gen_dirs = Dir.glob("#{TESTING_APP}/**/*").select { |e| Dir.exists? e }
+      rel_gen_dirs = gen_dirs.map { |dir| dir[(TESTING_APP.size+1)..-1] }
+
+      rel_dirs.sort.should eq rel_gen_dirs.sort
     end
 
   end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,5 @@
 require "spec"
 require "../src/kemalyst-generator/**"
+
+TESTING_APP = "../testing/testapp"
+APP_TPL_PATH = "./src/templates/app"
