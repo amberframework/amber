762feabdb3eda427a7a5eb8328b53010578ffc97
Elias J. Perez <elias.perez@harrys.com>
2017-04-13 17:43:59 -0400

    Made progress with router.

--- src/amber/request.cr
+++ src/amber/request.cr
@@ -1,12 +1,37 @@
 module Amber
-    class Request
-        getter method, path, handler
-        @handler : HTTP::Server::Context -> String
-        @http_verb : String
+   class Request
+    getter path
+    getter version
+    getter params
+    getter cookies
+    getter content_type
 
-        def initialize(@http_verb, @path : String, &handler : HTTP::Server::Context -> _)
-        @handler = ->(context : HTTP::Server::Context) do
-            handler.call(context).to_s
+    def initialize(request : HTTP::Request)
+        @path = request.path.not_nil!
+        @query = request.query
+        @version = request.version
+        @headers = request.headers
+        @cookies = {} of String => String
+        @content_type = request.headers["Content-type"]? || ""
+        @params = Hash(String, String).new
+    end
+
+    def initialize(request : HTTP::Request, params)
+        initialize(request)
+        @params = params ? params.not_nil! : Hash(String, String).new
+    end
+
+    # Adds parameters from param string
+    def add_params_from_string(params_string)
+        params = params_string.split("&")
+        return if params.size < 1
+        params.each do |x|
+        par = x.split("=")
+        next if par.size < 2
+        key = URI.unescape(par[0])
+        val = URI.unescape(par[1])
+        @params[key] = val
         end
+    end
     end
 end
