32e5185c60afdc829cd6aaef615a3e235e543188
Dru Jensen <drujensen@gmail.com>
2017-04-15 13:20:18 -0700

    controllers and mailers

--- src/templates/scaffold.cr
+++ src/templates/scaffold.cr
@@ -8,13 +8,15 @@ module Kemalyst::Generator
     @name : String
     @fields : Array(Field)
     @database : String
+    @language : String
     @timestamp : String
     @primary_key : String
 
     def initialize(@name, fields)
       @database = database
+      @language = language
       @fields = fields.map {|field| Field.new(field, database: @database)}
-      @fields += %w(created_at:time updated_at:time).map do |f| 
+      @fields += %w(created_at:time updated_at:time).map do |f|
         Field.new(f, hidden: true, database: @database)
       end
       @timestamp = Time.now.to_s("%Y%m%d%H%M%S")
@@ -25,6 +27,13 @@ module Kemalyst::Generator
       yaml_file = File.read("config/database.yml")
       yaml = YAML.parse(yaml_file)
       yaml.first.to_s
+    end
+
+    def language
+      return "slang" unless File.exists? ".kgen.yml"
+      yaml_file = File.read(".kgen.yml")
+      yaml = YAML.parse(yaml_file)
+      yaml["language"].to_s
     end
 
     def primary_key
