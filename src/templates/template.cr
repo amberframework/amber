ec7301111e0c790714f904ebcb3d123fdb106e5c
Dru Jensen <drujensen@gmail.com>
2017-04-15 09:47:04 -0700

    update to latest teeplate
    I needed to override a couple methods but now we are leveraging the
    features of teeplate.

--- src/templates/template.cr
+++ src/templates/template.cr
@@ -35,25 +35,64 @@ module Kemalyst::Generator
       case template
       when "app"
         puts "Rendering App #{name} in #{directory}"
-        App.new(name, @database, @language).render(directory)
+        App.new(name, @database, @language).render(directory, list: true, color: true)
       when "scaffold"
         puts "Rendering Scaffold #{name}"
-        Scaffold.new(name, fields).render(directory)
+        Scaffold.new(name, fields).render(directory, list: true, color: true)
       when "model"
         puts "Rendering Model #{name}"
-        Model.new(name, fields).render(directory)
+        Model.new(name, fields).render(directory, list: true, color: true)
       when "controller"
         puts "Rendering Controller #{name}"
-        Controller.new(name, fields).render(directory)
+        Controller.new(name, fields).render(directory, list: true, color: true)
       when "mailer"
         puts "Rendering Mailer #{name}"
-        Mailer.new(name, fields).render(directory)
+        Mailer.new(name, fields).render(directory, list: true, color: true)
       when "migration"
         puts "Rendering Migration #{name}"
-        Migration.new(name, fields).render(directory)
+        Migration.new(name, fields).render(directory, list: true, color: true)
       else
         raise "Template not found"
       end
     end
   end
 end
+
+class Teeplate::RenderingEntry
+  def appends?
+    @data.path.includes?("+")
+  end
+
+  def forces?
+    appends? || @data.forces? || @renderer.forces?
+  end
+
+  def local_path
+    @local_path ||= if appends?
+      @data.path.gsub("+","")
+    else
+      @data.path
+    end
+  end
+end
+
+module Teeplate
+  abstract class FileTree
+   # Renders all collected file entries.
+    #
+    # For more information about the arguments, see `Renderer`.
+    def render(out_dir, force : Bool = false, interactive : Bool = false, interact : Bool = false, list : Bool = false, color : Bool = false, per_entry : Bool = false, quit : Bool = true)
+      renderer = Renderer.new(out_dir, force: force, interact: interactive || interact, list: list, color: color, per_entry: per_entry, quit: quit)
+      entries = file_entries.reject{|entry| filter(entry) }
+      renderer << entries
+      renderer.render
+      renderer
+    end
+
+    # Override to filter files based on filename
+    def filter(entry)
+      return false
+    end
+  end
+end
+
