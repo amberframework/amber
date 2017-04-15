e79862b40c89668f77d22d16cd85087fdd26b475
Dru Jensen <drujensen@gmail.com>
2017-04-15 13:20:18 -0700

    controllers and mailers

--- src/templates/mailer.cr
+++ src/templates/mailer.cr
@@ -7,9 +7,18 @@ module Kemalyst::Generator
 
     @name : String
     @fields : Array(Field)
+    @language : String
 
     def initialize(@name, fields)
+      @language = language
       @fields = fields.map {|field| Field.new(field)}
+    end
+
+    def language
+      return "slang" unless File.exists? ".kgen.yml"
+      yaml_file = File.read(".kgen.yml")
+      yaml = YAML.parse(yaml_file)
+      yaml["language"].to_s
     end
   end
 end
