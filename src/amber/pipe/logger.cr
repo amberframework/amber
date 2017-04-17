c9a0e45e2ee9b23794e1aa11f2812a6c81ef72a1
Elias Perez <eliasjpr@gmail.com>
2017-04-16 20:37:13 -0400

    Logger Cleanup
    Why this change was necessary?
    - The logger was not that clean in the way it was putting to console
    
    How does this address the issue
    - Cleaned up the way we are doing logging.
    - Separates call into methods
    
    What side effects does this change have?
    - Colored output
    - Prints params

--- src/amber/pipe/logger.cr
+++ src/amber/pipe/logger.cr
@@ -4,19 +4,42 @@ require "logger"
 module Amber
   module Pipe
     class Logger < Base
+      getter log : ::Logger
+
       def self.instance
         @@instance ||= new
+      end
+
+      def initialize
+        @log = Amber::Server.instance.log
       end
 
       def call(context : HTTP::Server::Context)
         time = Time.now
         call_next(context)
-        status_code = context.response.status_code
-        method = context.request.method
-        resource = context.request.resource
+        status = context.response.status_code
         elapsed = elapsed_text(Time.now - time)
-        puts "#{status_code} | #{method} #{resource} | #{elapsed}"
+        @log.info "#{http_status(status)} | #{method(context)} #{path(context)} | #{elapsed}"
+        @log.info "Params: #{context.params.to_s.colorize(:yellow)}"
         context
+      end
+
+      def method(context)
+        context.request.method.colorize(:light_red).to_s + " "
+      end
+
+      def path(context)
+        "\"" + context.request.path.to_s.colorize(:yellow).to_s + "\" "
+      end
+
+      def http_status(status)
+        case status
+        when 200
+          text = "200 ".colorize(:green).to_s
+        when 404
+          text = "404 ".colorize(:red).to_s
+        end
+        "#{text}"
       end
 
       private def elapsed_text(elapsed)
