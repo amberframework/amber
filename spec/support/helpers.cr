ff6561318f4b838b71307b71b7e92a6789b35141
Elias Perez <eliasjpr@gmail.com>
2017-04-19 14:56:10 -0400

    Specs for session pipe
    Adds missing specs for the session pipe.

--- spec/support/helpers.cr
+++ spec/support/helpers.cr
@@ -7,3 +7,9 @@ def create_request_and_return_io(router, request)
   io.rewind
   HTTP::Client::Response.from_io(io, decompress: false)
 end
+
+def create_context(request)
+  io = IO::Memory.new
+  response = HTTP::Server::Response.new(io)
+  HTTP::Server::Context.new(request, response)
+end
