5e2aeebcc563f28509b4a677033c3e1eadcb8a9e
Elias Perez <eliasjpr@gmail.com>
2017-04-19 14:56:10 -0400

    Specs for session pipe
    Adds missing specs for the session pipe.

---
+++ spec/amber/pipe/session_spec.cr
@@ -0,0 +1,37 @@
+require "../../../spec_helper"
+
+module Amber
+  module Pipe
+    describe Session do
+      it "sets a cookie" do
+        request = HTTP::Request.new("GET", "/")
+        context = create_context(request)
+        session = Session.instance
+        session.call(context)
+        context.response.headers.has_key?("set-cookie").should be_true
+      end
+
+      it "encodes the session data" do
+        request = HTTP::Request.new("GET", "/")
+        context = create_context(request)
+        session = Session.instance
+        session.secret = "some-secret-key"
+        context.session["authorized"] = "true"
+        session.call(context)
+        cookie = context.response.headers["set-cookie"]
+        cookie.should eq "amber.session=8a45be69c9e296834650a6e73c50f931a60d6cf8--eyJhdXRob3JpemVkIjoidHJ1ZSJ9%0A; path=/"
+      end
+
+      it "uses a secret" do
+        request = HTTP::Request.new("GET", "/")
+        context = create_context(request)
+        session = Session.instance
+        session.secret = "some-secret-key"
+        context.session["authorized"] = "true"
+        session.call(context)
+        cookie = context.response.headers["set-cookie"]
+        cookie.should eq "amber.session=8a45be69c9e296834650a6e73c50f931a60d6cf8--eyJhdXRob3JpemVkIjoidHJ1ZSJ9%0A; path=/"
+      end
+    end
+  end
+end
