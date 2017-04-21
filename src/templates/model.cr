5d1c25c7ecaf60841d5fd47a46ec7b29bdff17a6
Dru Jensen <drujensen@gmail.com>
2017-04-20 17:07:44 -0700

    changes for new macros

--- src/templates/model.cr
+++ src/templates/model.cr
@@ -9,17 +9,35 @@ module Kemalyst::Generator
     @fields : Array(Field)
     @database: String
     @timestamp: String
+    @primary_key : String
 
     def initialize(@name, fields)
-      @fields = fields.map {|field| Field.new(field)}
       @database = database
+      @fields = fields.map {|field| Field.new(field, database: @database)}
+      @fields += %w(created_at:time updated_at:time).map do |f|
+        Field.new(f, hidden: true)
+      end
       @timestamp = Time.now.to_s("%Y%m%d%H%M%S")
+      @primary_key = primary_key
     end
 
     def database
       yaml_file = File.read("config/database.yml")
       yaml = YAML.parse(yaml_file)
       yaml.first.to_s
+    end
+
+    def primary_key
+      case @database
+      when "pg"
+        "id BIGSERIAL PRIMARY KEY"
+      when "mysql"
+        "id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY"
+      when "sqlite"
+        "id INTEGER NOT NULL PRIMARY KEY"
+      else
+        "id INTEGER NOT NULL PRIMARY KEY"
+      end
     end
   end
 end
