e0945edd50025f945f928f8272dc22017e42323e
Elias Perez <eliasjpr@gmail.com>
2017-04-25 06:29:33 -0400

    PR review changes
    - Added Amber::Extensions to encapsulate any DSLs and STDLib extensions.

---
+++ src/amber/extensions/core.cr
@@ -0,0 +1,11 @@
+require "./*"
+
+# We are patching the String class and Number struct to extend the predicates
+# available this will allow to add friendlier methods for validation cases.
+class String
+  include Amber::Extensions::StringExtension
+end
+
+abstract struct Number
+  include Amber::Extensions::NumberExtension
+end
