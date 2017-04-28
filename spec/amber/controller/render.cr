86793cebbad9562b5e609900145147e25366d0ff
Isaac Sloan <isaac@isaacsloan.com>
2017-04-28 13:31:24 -0600

    moved controller to factories

--- spec/amber/controller/render.cr
+++ spec/amber/controller/render.cr
@@ -1,19 +1,5 @@
 require "../../spec_helper"
 
-class TestController < Amber::Controller::Base
-  def render_template_page
-    render_template("spec/sample/views/test.slang")
-  end
-
-  def render_layout_too
-    render_both("spec/sample/views/test.slang", "spec/sample/views/layout.slang")
-  end
-
-  def render_both_inferred
-    render("test.slang", "layout.slang", "spec/sample/views", "./")
-  end
-end
-
 module Amber::Controller
   describe Amber::DSL::ControllerActions do 
     it "renders html from slang template" do
