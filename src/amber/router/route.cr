8a5dd41cd7d560588ea76fdeb45ec0d10caa03df
Elias J. Perez <elias.perez@harrys.com>
2017-05-01 09:44:47 -0400

    Attachs before/after callbacks to request lifecycle
    - For each request it automatically calls the before and
    after callback hooks.
    
    Changes
    - Adds property to route.cr for the name of the action.
    - Adds #call(context) to the route class

--- src/amber/router/route.cr
+++ src/amber/router/route.cr
@@ -1,18 +1,21 @@
 module Amber
   class Route
-    property :controller, :handler, :verb, :resource, :valve, :params
-    getter controller : Controller::Base
+    property :controller, :handler, :action, :verb, :resource, :valve, :params
 
-    def initialize(verb : String | Symbol,
-                   resource : String,
-                   controller = Controller::Base.new,
-                   handler : Proc(String) = ->{ "500" },
-                   valve : Symbol = :web)
-      @verb = verb
-      @resource = resource
-      @controller = controller
-      @handler = handler
-      @valve = valve
+    def initialize(@verb : String | Symbol,
+                   @resource : String,
+                   @controller = Controller::Base.new,
+                   @handler : Proc(String) = ->{ "500" },
+                   @action : Symbol = :index,
+                   @valve : Symbol = :web)
+    end
+
+    def call(context)
+      controller.set_context(context)
+      controller.run_before_filter(action)
+      content = handler.call
+      controller.run_after_filter(action)
+      content
     end
   end
 end
