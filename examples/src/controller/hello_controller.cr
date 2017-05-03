96375f1c3450f3cf617defda26428076b7038e75
Elias Perez <eliasjpr@gmail.com>
2017-05-03 19:42:14 -0400

    Fixes Example Demo
    Issue:[ch113]
    
    This change was necessary to fix the Example demo site.
    
    It fixes the demo and uses the new added features such as params
    validationss, filters and template rendering
    
    Now we have a functional demo example with new features.

---
+++ examples/src/controller/hello_controller.cr
@@ -0,0 +1,40 @@
+class HelloController < Amber::Controller::Base
+  @num = 0
+
+  # Filters are methods that are run "before", "after" a controller action.
+  before_action do
+    only [:index, :world, :show] { increment(1) }
+    only :index { increment(1) }
+  end
+
+  after_action do
+    only [:index, :world] { increment(1) }
+  end
+
+  def index
+    "Hello from no where, Increment result: #{@num}"
+  end
+
+  def world
+    if params.valid?
+      # "Welcome to planet: #{params["planet"]}!"
+      "Valid!"
+    else
+      "There is no world defined!"
+    end
+  end
+
+  def template
+    render "hello.slang"
+  end
+
+  def increment(n)
+    @num += n
+  end
+
+  def hello_params
+    params.validation do
+      required(:planet) { |w| !w.empty? }
+    end
+  end
+end
--- src/amber/router/route.cr
+++ src/amber/router/route.cr
@@ -3,16 +3,22 @@ module Amber
     property :controller, :handler, :verb, :resource, :valve, :params
     getter controller : Controller::Base
 
-    def initialize(verb : String | Symbol,
-                   resource : String,
-                   controller = Controller::Base.new,
-                   handler : Proc(String) = ->{ "500" },
-                   valve : Symbol = :web)
-      @verb = verb
-      @resource = resource
-      @controller = controller
-      @handler = handler
-      @valve = valve
+    def initialize(@verb : String,
+                   @resource : String,
+                   @controller = Controller::Base.new,
+                   @handler : Proc(String) = ->{ "500" },
+                   @action : Symbol = :index,
+                   @valve : Symbol = :web)
+    end
+
+    def call(context)
+      controller.set_context(context)
+      controller.run_before_filter(:all)
+      controller.run_before_filter(action)
+      content = handler.call
+      controller.run_after_filter(action)
+      controller.run_after_filter(:all)
+      content
     end
   end
 end
