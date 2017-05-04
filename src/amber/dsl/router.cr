1502775a6e433091dd4aedff1674b3d0cd5f21ac
Elias J. Perez <elias.perez@harrys.com>
2017-05-04 13:02:51 -0400

    Routing Resources
    Issue:  [Restful Resources [ch43]](https://app.clubhouse.io/amber/story/43/routes-resources)
    
    Allows for generating all resources for a given controller.
    
    With one line of code developers can now  define resources bases on the
    HTTP verbs GET, PUT, POST, DELETE, PATCH.

--- src/amber/dsl/router.cr
+++ src/amber/dsl/router.cr
@@ -6,7 +6,12 @@ module Amber::DSL
   end
 
   record Router, router : Pipe::Router, valve : Symbol, scope : String do
+    RESOURCES = [:get, :post, :put, :patch, :delete, :options, :head, :trace, :connect]
+
+
     macro route(verb, resource, controller, action)
+
+      puts "{{verb.id}}  {{resource.id}}   {{controller.id}}   {{action.id}}"
       %controller = {{controller.id}}.new
       %handler = ->%controller.{{action.id}}
       %verb = {{verb.upcase.id.stringify}}
@@ -15,12 +20,24 @@ module Amber::DSL
       router.add(%route)
     end
 
-    {% for verb in {:get, :post, :put, :delete, :options, :head, :trace, :connect} %}
+    {% for verb in RESOURCES %}
 
       macro {{verb.id}}(*args)
         route {{verb}}, \{{*args}}
       end
 
     {% end %}
+
+    # TODO Clean this up
+    macro resources(path, controller, actions = [:index, :edit, :new, :show, :create, :update, :put, :delete])
+      get "{{path.id}}", {{controller}}, :index if {{actions}}.includes?(:index)
+      get "{{path.id}}/:id/edit", {{controller}}, :edit  if {{actions}}.includes?(:edit)
+      get "{{path.id}}/new", {{controller}}, :new if {{actions}}.includes?(:new)
+      get "{{path.id}}/:id", {{controller}}, :show if {{actions}}.includes?(:show)
+      post "{{path.id}}", {{controller}}, :create if {{actions}}.includes?(:create)
+      patch "{{path.id}}/:id", {{controller}}, :update if {{actions}}.includes?(:update)
+      put "{{path.id}}/:id", {{controller}}, :update if {{actions}}.includes?(:update)
+      delete "{{path.id}}/:id", {{controller}}, :delete if {{actions}}.includes?(:delete)
+    end
   end
 end
