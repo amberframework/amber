b632fbcff1f688fbc6b3b96ec514afbfd43a311e
Dru Jensen <drujensen@gmail.com>
2017-05-07 21:37:23 -0700

    update docker-compose.  Add docker-sidekiq.yml

--- src/templates/job.cr
+++ src/templates/job.cr
@@ -7,9 +7,24 @@ module Kemalyst::Generator
 
     @name : String
     @fields : Array(Field)
+    @database : String
+    @db_url : String
 
     def initialize(@name, fields)
+      @database = database
+      @db_url = ""
       @fields = fields.map {|field| Field.new(field)}
+    end
+
+    DATABASE_YML = "config/database.yml"
+    def database
+      if File.exists?(DATABASE_YML) &&
+        (yaml = YAML.parse(File.read DATABASE_YML)) &&
+        (database = yaml.first)
+        database.to_s
+      else
+        return "pg"
+      end
     end
   end
 end
