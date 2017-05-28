7678de4039c37af1669dbbc0d33800e6866dffda
Isaac Sloan <isaac@isaacsloan.com>
2017-05-27 23:46:44 -0600

    changes to application.cr

--- src/templates/app/config/application.cr
+++ src/templates/app/config/application.cr
@@ -1,8 +1,8 @@
 Amber::Server.instance.config do |app|
   # Server options
   app_path = __FILE__          # Do not change unless you understand what you are doing.
-  app.name = "Five minute blog." # A descriptive name for your app
-  app.port = 4000              # Port you wish your app to run
+  app.name = "#{@name.capitalize} web application."
+  app.port = 3000              # Port you wish your app to run
   app.env = "development".colorize(:yellow).to_s
   app.log = ::Logger.new(STDOUT)
   app.log.level = ::Logger::INFO
