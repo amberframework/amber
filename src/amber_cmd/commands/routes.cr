e581e84598e74d4e92d674dd7913ce3850bd558a
Isaac Sloan <isaac@isaacsloan.com>
2017-06-10 16:27:03 -0700

    Fast Routes Command (#26)
    * Adds Routes Command
    
    - In progress
    
    * Working amber routes that parses the routes file. Very fast because nothing needs to compile!
    
    * added scopes and slimmed down case statement as suggested
    
    * moved regex to constants
    
    * Create README.md (#28)
    
    * Delete formula from repo (#29)
    
    - Homebrew formula can be found https://github.com/Amber-Crystal/homebrew-amber
    
    * Fixes ENV and CONST variables (#22)
    
    - It first sets the ENV and CONST variables in ./src/{{name}}.cr
    - In app/config uses the constants if defined
    
    * changed some things according to suggestions dried some things up and used more descriptive names
    
    * Bumped version number to v0.1.14.

--- src/amber_cmd/commands/routes.cr
+++ src/amber_cmd/commands/routes.cr
@@ -4,17 +4,29 @@ require "sentry/sentry_command"
 
 module Amber::CMD
   class MainCommand < Cli::Supercommand
-
     class Routes < Sentry::SentryCommand
+      RESOURCE_ROUTE_REGEX = /(\w+)\s+\"([^\"]+)\",\s*(\w+)(?:,\s*(\w+)\:\s*\[([^\]]+)\])?/
+      VERB_ROUTE_REGEX     = /(\w+)\s+\"([^\"]+)\",\s*(\w+),\s*:(\w+)/
+      PIPE_SCOPE_REGEX     = /routes\s+\:(\w+)(?:,\s+\"([^\"]+)\")?/
+
+      LABELS         = ["Verb", "Controller", "Action", "Pipeline", "Scope", "URI Pattern"]
+      ACTION_MAPPING = {
+        "get" => ["index", "show", "new", "edit"],
+        "post" => ["create"], "patch" => ["update"],
+        "put" => ["update"], "delete" => ["destroy"],
+      }
+
       command_name "routes"
-      getter routes : JSON::Any?
+      getter routes = Array(Hash(String, String)).new
+      property current_pipe : String?
+      property current_scope : String?
 
       class Help
         caption "# Print out all defined routes in match order, with names"
       end
 
       def run
-        @routes = get_routes
+        parse_routes
         print_routes_table
       rescue
         puts "Error: Not valid project root directory.".colorize(:red)
@@ -23,30 +35,83 @@ module Amber::CMD
         exit 1
       end
 
-      private def get_routes
-        code = <<-CODE
-          require "amber"
-          require "./src/**"
+      private def parse_routes
+        File.read_lines("config/routes.cr").each do |line|
+          case line.strip
+          when .starts_with?("routes")
+            set_pipe(line)
+          when .starts_with?("resources")
+            set_resources(line)
+          else
+            set_route(line)
+          end
+        end
+      end
 
-          AMBER_CMD_PROCESS_ROUTES = true
-          puts Amber::Server.routes.to_json
-        CODE
-        routes_json = `crystal eval #{code.gsub("\n", ";").inspect}`
-        return JSON.parse(routes_json) unless routes_json.empty?
-        puts "No routes defined!"
+      private def set_route(route_string)
+        if route_match = route_string.to_s.match(VERB_ROUTE_REGEX)
+          return unless ACTION_MAPPING.keys.includes?(route_match[1]?.to_s)
+          build_route(
+            verb: route_match[1]?, controller: route_match[3]?,
+            action: route_match[4]?, pipeline: current_pipe,
+            scope: current_scope, uri_pattern: route_match[2]?
+          )
+        end
+      end
+
+      private def set_resources(resource_string)
+        if route_match = resource_string.to_s.match(RESOURCE_ROUTE_REGEX)
+          filter = route_match[4]?
+          filter_actions = route_match[5]?.to_s.gsub(/\:|\s/, "").split(",")
+          ACTION_MAPPING.each do |verb, v|
+            v.each do |action|
+              case filter
+              when "only"
+                next unless filter_actions.includes?(action)
+              when "except"
+                next if filter_actions.includes?(action)
+              end
+              build_route(
+                verb: verb, controller: route_match[3]?, action: action,
+                pipeline: current_pipe, scope: current_scope,
+                uri_pattern: build_uri_pattern(route_match[2]?, action, current_scope)
+              )
+            end
+          end
+        end
+      end
+
+      def build_route(verb, uri_pattern, controller, action, pipeline, scope = "")
+        route = {"Verb" => verb.to_s}
+        route["URI Pattern"] = uri_pattern.to_s
+        route["Controller"] = controller.to_s
+        route["Action"] = action.to_s
+        route["Pipeline"] = pipeline.to_s
+        route["Scope"] = scope.to_s
+        routes << route
+      end
+
+      private def build_uri_pattern(route, action, scope)
+        route_end = {"show" => ":id", "new" => "new", "edit" => ":id/edit", "update" => ":id", "destroy" => ":id"}
+        [scope, route, route_end[action]?].compact.join("/").gsub("//", "/")
+      end
+
+      private def set_pipe(pipe_string)
+        if route_match = pipe_string.to_s.match(PIPE_SCOPE_REGEX)
+          @current_pipe = route_match[1]?
+          @current_scope = route_match[2]?
+        end
       end
 
       private def print_routes_table
         table = ShellTable.new
-        table.labels = [
-          "Verb", "Controller", "Action", "Pipeline", "Scope", "Resource"
-        ]
+        table.labels = LABELS
         table.label_color = :light_red
         table.border_color = :dark_gray
-        routes.not_nil!.each do |k, v|
+        routes.each do |route|
           row = table.add_row
-          JSON.parse(v.to_s).each do |col, val|
-            row.add_column val.to_s
+          LABELS.each do |l|
+            row.add_column route[l].to_s
           end
         end
         puts table
--- src/amber_cmd/version.cr
+++ src/amber_cmd/version.cr
@@ -1,3 +1,3 @@
 module Amber::CMD
-  VERSION = "0.1.13"
+  VERSION = "0.1.14"
 end
