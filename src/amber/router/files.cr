645d9316813638d62fd16e44cee2927478895216
Elias Perez <eliasjpr@gmail.com>
2017-06-11 07:43:22 -0400

    Apply crystal format

--- src/amber/router/files.cr
+++ src/amber/router/files.cr
@@ -34,4 +34,3 @@ module Amber::Router
     end
   end
 end
-
--- src/amber/router/pipe/base.cr
+++ src/amber/router/pipe/base.cr
@@ -5,6 +5,7 @@ module Amber
     # Base class to include the HTTP::Handler module.
     class Base
       include HTTP::Handler
+
       # Execution of this handler.
       def call(context : HTTP::Server::Context)
         call_next context
