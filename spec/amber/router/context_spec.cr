6850f02d3bf8dce39cfc5ae7a80e35fc4e9e4521
Elias Perez <eliasjpr@users.noreply.github.com>
2017-06-14 19:00:03 -0400

    HTTP Header Override (#117)
    * HTTP Header Override
    
    When deploying REST API based web services, you may encounter access
    limitations on both the server and client sides. On the server side, the
    access to your REST based web application might be done via a proxy web
    server that does not support certain HTTP operations such as DELETE or
    PUT for security reasons.
    
    Such requests will lead to HTTP 405 error:
    Http Status Code: 405
    Reason: Request method is not allowed by the server
    
    This PR adds support to offer these operations via POST operations and
    ask your clients to add specific X-HTTP method override field in the
    header of their requests.
    
    zrt spec/amber/router/params.cr:53
    
    * fixup! HTTP Header Override

--- spec/amber/router/context_spec.cr
+++ spec/amber/router/context_spec.cr
@@ -1,6 +1,83 @@
 require "../../../spec_helper"
 
 describe HTTP::Server::Context do
+  describe "#override_request_method!" do
+    describe "when X-HTTP-Method-Overrid is present" do
+      it "overrides form POST method to PUT, PATCH, DELETE" do
+        %w(PUT PATCH DELETE).each do |method|
+          header = HTTP::Headers.new
+          header[HTTP::Server::Context::OVERRIDE_HEADER] = method
+          request = HTTP::Request.new("POST", "/?test=test", header)
+
+          context = create_context(request)
+
+          context.request.method.should eq method
+        end
+      end
+
+      it "overrides form GET method to PUT, PATCH, DELETE" do
+        %w(PUT PATCH DELETE).each do |method|
+          header = HTTP::Headers.new
+          header[HTTP::Server::Context::OVERRIDE_HEADER] = method
+          request = HTTP::Request.new("GET", "/?test=test", header)
+
+          context = create_context(request)
+
+          context.request.method.should eq method
+        end
+      end
+
+      it "takes form post over header override" do
+        %w(PUT PATCH DELETE).each do |method|
+          header = HTTP::Headers.new
+          header[HTTP::Server::Context::OVERRIDE_HEADER] = method
+          header["content-type"] = "application/x-www-form-urlencoded"
+          request = HTTP::Request.new("GET", "/?test=test", header, "_method=PATCH")
+
+          context = create_context(request)
+
+          context.request.method.should eq "PATCH"
+        end
+      end
+    end
+
+    it "overrides form POST method to PUT, PATCH, DELETE" do
+      %w(PUT PATCH DELETE).each do |method|
+        header = HTTP::Headers.new
+        header["content-type"] = "application/x-www-form-urlencoded"
+        request = HTTP::Request.new("POST", "/?test=test", header, "_method=#{method}")
+
+        context = create_context(request)
+
+        context.request.method.should eq method
+      end
+    end
+
+    it "overrides form GET method to PUT, PATCH, DELETE" do
+      %w(PUT PATCH DELETE).each do |method|
+        header = HTTP::Headers.new
+        header["content-type"] = "application/x-www-form-urlencoded"
+        request = HTTP::Request.new("GET", "/?test=test", header, "_method=#{method}")
+
+        context = create_context(request)
+
+        context.request.method.should eq method
+      end
+    end
+
+    it "does not override other than PUT, PATCH, DELETE" do
+      %w(PUT PATCH DELETE).each do |method|
+        header = HTTP::Headers.new
+        header["content-type"] = "application/x-www-form-urlencoded"
+        request = HTTP::Request.new("HEAD", "/?test=test", header, "_method=#{method}")
+
+        context = create_context(request)
+
+        context.request.method.should eq "HEAD"
+      end
+    end
+  end
+
   it "parses query params" do
     request = HTTP::Request.new("GET", "/?test=test")
 
