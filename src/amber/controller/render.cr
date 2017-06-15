262ec5428c5e97964a22a42684e871851b8417a4
Isaac Sloan <isaac@isaacsloan.com>
2017-06-15 11:23:58 -0600

    fixed render ecr partials from inside templates (#118)

--- src/amber/controller/render.cr
+++ src/amber/controller/render.cr
@@ -2,10 +2,6 @@ module Amber::Controller
   module Render
     LAYOUT = "application.slang"
 
-    macro set_layout(filename)
-      LAYOUT = {{filename}}
-    end
-
     macro render_template(filename, path = "src/views")
       {% if filename.id.split("/").size > 2 %}
         Kilt.render("{{filename.id}}")
@@ -15,10 +11,15 @@ module Amber::Controller
     end
 
     macro render(filename, layout = true, path = "src/views", folder = __FILE__)
+      # NOTE: content is basically yield rails layouts.
       {% if filename.id.split("/").size > 1 %}
         content = render_template("#{{{filename}}}", {{path}})
       {% else %}
-        content = render_template("#{{{folder.split("/").last.gsub(/\_controller\.cr|\.cr/, "")}}}/#{{{filename}}}", {{path}})
+        {% if folder.id.ends_with?(".ecr") %}
+          content = render_template("#{{{folder.split("/")[-2]}}}/#{{{filename}}}", {{path}})
+        {% else %}
+          content = render_template("#{{{folder.split("/").last.gsub(/\_controller\.cr|\.cr/, "")}}}/#{{{filename}}}", {{path}})
+        {% end %}
       {% end %}
 
       {% if layout && !filename.id.split("/").last.starts_with?("_") %}
