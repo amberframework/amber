10e5cede46f81d347bd90eb3c6cf7c35d5ad4077
Isaac Sloan <isaac@isaacsloan.com>
2017-06-20 20:27:33 -0600

    provision and deploy work so well :)

--- src/amber_cmd/commands/deploy.cr
+++ src/amber_cmd/commands/deploy.cr
@@ -28,38 +28,39 @@ module Amber::CMD
 
       class Options
         bool "--init"
-        arg "server_suffix", desc: "# Name of server.", default: "production" 
+        arg "server_suffix", desc: "# Name of server.", default: "production"
         string ["-s", "--service"], desc: "# Deploy to cloud service: digitalocean | heroku | aws | azure", default: "digitalocean"
         string ["-k", "--key"], desc: "# API Key for service"
-        string ["-t", "-b"], desc: "# branch or tag", default: "master"
+        string ["-t", "--tag"], desc: "# Tag to use. Default master."
+        string ["-b", "--branch"], desc: "# Branch to use. Default master.", default: "master"
       end
 
       def provision
         puts "Provisioning server #{server_name}"
-        # create_cloud_server
-        # create_swapfile
-        # create_deploy_keys
-        # clone_project
-        # remote_cmd "docker build -t amberimage amberproject"
-        update_project
+        create_cloud_server
+        create_swapfile
+        create_deploy_keys
+        clone_project
+        setup_project
         deploy_project
-        # update_project
-        # setup_project
-        ip = `docker-machine ip #{server_name}`.strip
-        puts "ssh root@#{ip} -i ~/.docker/machine/machines/#{server_name}/id_rsa"
+        display_helper_links
       end
 
       def deploy
         puts "Deploying latest changes to server #{server_name}"
-        # checkout_project
         update_project
+        deploy_project
+        display_helper_links
+      end
+
+      def display_helper_links
         ip = `docker-machine ip #{server_name}`.strip
         puts "ssh root@#{ip} -i ~/.docker/machine/machines/#{server_name}/id_rsa"
         puts "open http://#{ip}"
       end
 
       def create_deploy_keys
-        remote_cmd(%Q(bash -c "echo | ssh-keygen -q -N '' -t rsa -b 4096 -C 'deploy@#{project_name}'")) 
+        remote_cmd(%Q(bash -c "echo | ssh-keygen -q -N '' -t rsa -b 4096 -C 'deploy@#{project_name}'"))
         puts "\nPlease add this to your projects deploy keys on github or gitlab:"
         puts remote_cmd("tail .ssh/id_rsa.pub")
         puts "\n"
@@ -76,6 +77,8 @@ module Amber::CMD
       def create_cloud_server
         puts "Deploying #{@server_name}"
         puts "Creating docker machine: #{@server_name.colorize(:blue)}"
+        puts "Enter your write enabled Digital Ocean API KEY or create on with the link below."
+        puts "https://cloud.digitalocean.com/settings/api/tokens/new"
         do_token = options.key? || getsecret("DigitalOcean Token")
         `docker-machine create #{@server_name} --driver=digitalocean --digitalocean-access-token=#{do_token}`
         puts "Done creating machine!"
@@ -99,43 +102,45 @@ module Amber::CMD
         puts "please enter repo to deploy from"
         puts "example: git@github.com/you/project.git"
         repo = gets
-        remote_cmd(%Q(bash -c "echo | git clone #{repo} amberproject"))
+        remote_cmd(%Q("ssh-keyscan github.com >> ~/.ssh/known_hosts"))
+        remote_cmd(%Q(bash -c "yes yes | git clone #{repo} amberproject"))
       end
 
       def setup_project
         puts "deploying project"
-        remote_cmd "docker network create --driver bridge ambernet"
-        remote_cmd "docker build -t amberimage -f amberproject/config/deploy/Dockerfile amberproject"
-        remote_cmd "docker run -it --name amberdb -v /root/db_volume:/var/lib/postgres/data --network=ambernet -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=password -e POSTGRES_DB=crystaldo_development -d postgres"
-        remote_cmd "docker run -it --name amberweb -v /root/amberproject:/app/user -p 80:3000 --network=ambernet -e DATABASE_URL=postgres://admin:password@amberdb:5432/crystaldo_development -d amberimage"
-        deploy_project
+        parallel(
+          remote_cmd("docker network create --driver bridge ambernet"),
+          remote_cmd("docker build -t amberimage -f amberproject/config/deploy/Dockerfile amberproject")
+        )
+        parallel(
+          remote_cmd("docker run -it --name amberdb -v /root/db_volume:/var/lib/postgresql/data --network=ambernet -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=password -e POSTGRES_DB=crystaldo_development -d postgres"),
+          remote_cmd("docker run -it --name amberweb -v /root/amberproject:/app/user -p 80:3000 --network=ambernet -e DATABASE_URL=postgres://admin:password@amberdb:5432/crystaldo_development -d amberimage")
+        )
       end
 
       def update_project
-        remote_cmd %Q(bash -c "cd amberproject && git pull")
+        checkout_string = options.tag? ? "tags/#{options.tag?}" : options.branch
+        remote_cmd %Q(bash -c "cd amberproject && git pull && git checkout #{checkout_string}")
       end
 
       def deploy_project
-        setup_project if remote_cmd("docker ps").split("\n").size < 4
+        setup_project if remote_cmd("docker ps").count("\n") < 3
         remote_cmd "docker exec -i amberweb crystal deps update"
         remote_cmd "docker exec -i amberweb amber migrate up"
-        remote_cmd "docker exec -i amberweb crystal build src/#{project_name}.cr" 
-        remote_cmd "docker exec -i amberweb killall #{project_name}" 
-        remote_cmd %Q(docker exec -id amberweb ./#{project_name})
+        remote_cmd "docker exec -i amberweb crystal build src/#{project_name}.cr"
+        remote_cmd "docker exec -i amberweb killall #{project_name}"
+        remote_cmd "docker exec -id amberweb ./#{project_name}"
       end
 
       def stop_and_remove
-        cmds = ["docker stop amberweb"]
-        cmds << "docker rm amberweb"
-        # cmds << "docker stop amberdb"
-        # cmds << "docker rm amberdb"
+        cmds = ["docker rm -rf amberweb"]
+        cmds << "docker rm -rf amberdb"
         remote_cmd(%Q(bash -c "#{cmds.join(" && ")}"))
       end
 
       def update_project
         remote_cmd(%Q("cd amberproject && git pull"))
       end
-
     end
   end
 end
