e4098db9af858f287bbda5c7a1123620dff7e1c9
Isaac Sloan <isaac@isaacsloan.com>
2017-06-30 23:47:19 -0700

    Changed filename to match class name. (#123)
    Tests passed for 4 days. Recent crystal update broke travis though.

--- src/amber/support/message_verifier.cr
+++ src/amber/support/message_verifier.cr
@@ -8,16 +8,16 @@ module Amber::Support
     end
 
     def valid_message?(signed_message)
-      splitted = signed_message.to_s.split("--", 2)
-      return if splitted.size < 2
-      data, digest = splitted
+      split_message = signed_message.to_s.split("--", 2)
+      return if split_message.size < 2
+      data, digest = split_message
       data.size > 0 && digest.size > 0 && Crypto::Subtle.constant_time_compare(digest, generate_digest(data))
     end
 
     def verified(signed_message : String)
       if valid_message?(signed_message)
         begin
-          data = signed_message.split("--")[0]
+          data = signed_message.split("--").first
           String.new(decode(data))
         rescue argument_error : ArgumentError
           return if argument_error.message =~ %r{invalid base64}
