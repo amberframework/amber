1975e7f89f5ba73845b306fd82b80dc41aca5022
Elias Perez <eliasjpr@users.noreply.github.com>
2017-08-12 20:42:46 -0400

    Adds Flash to Current Redirect Implementation (#172)
    * Adds Flash to Current Redirect Implementation
    
    Issue: https://github.com/amber-crystal/amber/issues/76
    
    * This adds flash to the redirect_to method
    * Arguments should be passed as named params
    
    * fixup! Adds Flash to Current Redirect Implementation
    
    * fixup! Adds Flash to Current Redirect Implementation
    
    * fixup! Adds Flash to Current Redirect Implementation

--- src/amber/controller/redirect.cr
+++ src/amber/controller/redirect.cr
@@ -1,27 +1,32 @@
 module Amber::Controller
   # This class writes the redirect URL to the response headers and parses params.
   class LocationRedirect
-    getter location, status, query_params
+    getter location, status, params, flash
 
-    def initialize(@location : String,
-                   @status = 302,
-                   @query_params : Hash(String, String)? = nil)
+    @location : String
+    @status : Int32 = 302
+    @params : Hash(String, String)? = nil
+    @flash : Hash(String, String)? = nil
+
+    def initialize(@location, @status = 302, @params = nil, @flash = nil)
       raise_redirect_error(location) if location.empty?
     end
 
-    def redirect(for response)
+    def redirect(for context)
+      response = context.response
+      context.flash.merge!(flash.not_nil!) if !flash.nil?
       raise_redirect_error(location) unless location.url? || location.chars.first == '/'
-      set_location(response, location, status, query_params)
+      set_location(response, location, status, params)
     end
 
-    private def set_location(response, location, status = 302, query_params : _ = nil)
-      url_path = encode_query_string(location, query_params)
+    private def set_location(response, location, status = 302, params : _ = nil)
+      url_path = encode_query_string(location, params)
       response.headers.add "Location", url_path
       response.status_code = status
     end
 
-    def encode_query_string(location, query_params)
-      return location + "?" + HTTP::Params.encode(query_params).to_s if query_params
+    def encode_query_string(location, params)
+      return location + "?" + HTTP::Params.encode(params).to_s if params
       location
     end
 
@@ -31,26 +36,25 @@ module Amber::Controller
   end
 
   module RedirectFactory
-    def redirect_to(location : String, *args)
-      LocationRedirect.new(location, *args).redirect(response)
+    def redirect_to(location : String, **args)
+      LocationRedirect.new(location, **args).redirect(context)
       halt!(response.status_code, "Redirecting to #{response.headers["Location"]}")
     end
 
     # Redirects to the specified controller, action
-    def redirect_to(controller : Symbol, action : Symbol, *args)
-      LocationRedirect.new("/#{controller}/#{action}", *args).redirect(response)
+    def redirect_to(controller : Symbol, action : Symbol, **args)
+      LocationRedirect.new("/#{controller}/#{action}", **args).redirect(context)
       halt!(response.status_code, "Redirecting to #{response.headers["Location"]}")
     end
 
     # Redirects within the same controller
-    def redirect_to(action : Symbol, *args)
-      controller = self.class.to_s.chomp("Controller").downcase
-      LocationRedirect.new("/#{controller}/#{action}", *args).redirect(response)
+    def redirect_to(action : Symbol, **args)
+      LocationRedirect.new("/#{controller_name}/#{action}", **args).redirect(context)
       halt!(response.status_code, "Redirecting to #{response.headers["Location"]}")
     end
 
-    def redirect_back(*args)
-      LocationRedirect.new(request.headers["Referer"]).redirect(response)
+    def redirect_back(**args)
+      LocationRedirect.new(request.headers["Referer"], status: 302).redirect(context)
       halt!(response.status_code, "Redirecting to #{response.headers["Location"]}")
     end
   end
