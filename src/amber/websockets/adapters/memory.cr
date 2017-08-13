ee74dba7321a4e42291b22608fe762fe61009342
Nick Franken <shnick@gmail.com>
2017-08-13 17:21:23 -0500

    passing client_socket to handle_message

--- src/amber/websockets/adapters/memory.cr
+++ src/amber/websockets/adapters/memory.cr
@@ -1,16 +1,16 @@
 module Amber::WebSockets::Adapters
   # The MemoryAdapter is intended for development use only and shouldn't be used in production.
   class MemoryAdapter
-    @listeners = Array(NamedTuple(path: String, listener: Proc(JSON::Any, Nil))).new
+    @listeners = Array(NamedTuple(path: String, listener: Proc(String, JSON::Any, Nil))).new
 
     def self.instance
       @@instance ||= new
     end
 
     # On *message* publish, just call all listeners procs
-    def publish(topic_path, message)
+    def publish(topic_path, client_socket, message)
       spawn do
-        @listeners.select { |l| l[:path] == topic_path }.each { |l| l[:listener].call(message) }
+        @listeners.select { |l| l[:path] == topic_path }.each { |l| l[:listener].call(client_socket.id, message) }
       end
     end
 
