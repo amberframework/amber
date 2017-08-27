f9cbf16ba153b69259c456052835085be00e9563
Isaac Sloan <isaac@isaacsloan.com>
2017-08-27 01:31:03 -0600

    tests for controller generator

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -8,3 +8,11 @@ require "./amber/core/controller/*"
 TESTING_APP  = "./testapp"
 APP_TPL_PATH = "./src/amber/cmd/templates/app"
 CURRENT_DIR  = Dir.current
+
+module Amber::CMD::Spec
+  def self.cleanup
+    puts "cleaning up..."
+    Dir.cd(CURRENT_DIR)
+    `rm -rf #{TESTING_APP}`
+  end
+end
