b43713f80f58865d7878bd570740c4b1d2bac8e0
Elias Perez <eliasjpr@users.noreply.github.com>
2017-08-31 22:41:57 -0400

    Refactors Amber::Server (#209)
    * Removes and Adds helper server methods
    
    - Why is this change necessary?
    
    Some of the calls to the server instance was invoking multiple methods
    for example Amber::Server.instance.pubsub_adapter.instance, with the
    changes this now reads as Amber::Server.pubsub_adapter making it easy to
    change without reworking the caller.
    
    - How does it address the issue
    
    The resulting changes tends to be more maintainable and adaptable making
    the code more readable to the developer.
    
    - What side effects does this change have?o
    
    Narrow interfaces, giving access to only as much information as it needs to do
    its job, as each method needs to know about a small set of methods of closely related objects
    
    High number of class methods
    
    * fixup! Removes and Adds helper server methods
    
    * fixup! Removes and Adds helper server methods
    
    * fixup! Removes and Adds helper server methods
    
    * fixup! Removes and Adds helper server methods
    
    * fixup! Removes and Adds helper server methods
    
    * fixup! Removes and Adds helper server methods

--- src/amber/websockets/subscription_manager.cr
+++ src/amber/websockets/subscription_manager.cr
@@ -14,7 +14,7 @@ module Amber
         when "message" then message client_socket, message
         when "leave"   then leave client_socket, message
         else
-          Amber::Server.instance.log.error "Uncaptured event #{event}"
+          Amber::Server.log.error "Uncaptured event #{event}"
         end
       end
 
