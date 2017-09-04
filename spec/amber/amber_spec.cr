720b91e515f27b53592b9e801532dcb7efb40f47
Elias Perez <eliasjpr@users.noreply.github.com>
2017-09-04 03:25:54 -0400

    Bug Fixe: Socket endpoint is not defined method error (#227)
    When defining a socket endpoint it withing route it currently
    throws an error socket_endpoint method is not defined. This is due to
    the fact that the socket_endpoint is not accessible through the routes
    
    In order to correct the bug a websocket method was defined in place of
    socket_endpoint in DSL::Router
    
    Changes:
    
    - Adds websocket as a  method instead of macro to Router DSL (See #224)
    - Adds specs to ensure socket endpoint are defined
    - Removes socker_endpoint from Amber::Server
    - Makes socket_routes public
    
    This changes the current API from socket_endpoint to websocket since
    this naming was to honor proposed changes in
    https://github.com/amber-crystal/amber/pull/224

--- spec/amber/amber_spec.cr
+++ spec/amber/amber_spec.cr
@@ -1,5 +1,9 @@
 require "../../spec_helper"
 
+struct UserSocket < Amber::WebSockets::ClientSocket; end
+
+struct RoomSocket < Amber::WebSockets::ClientSocket; end
+
 describe Amber::Server do
   describe ".configure" do
     it "it loads environment settings from test.yml" do
@@ -58,6 +62,28 @@ describe Amber::Server do
       }
       settings.secrets.should eq expected_secrets
       settings.secret_key_base.should eq "ox7cTo_408i4WZkKZ_5OZZtB5plqJYhD4rxrz2hriA4"
+    end
+
+    it "defines socket endpoint" do
+      Amber::Server.settings.router.socket_routes = [] of NamedTuple(path: String, handler: Amber::WebSockets::Server::Handler)
+
+      Amber::Server.configure do |app|
+        pipeline :web do
+        end
+
+        routes :web do
+          websocket "/user", UserSocket
+          websocket "/room", RoomSocket
+        end
+      end
+
+      router = Amber::Server.settings.router
+      websockets = router.socket_routes
+
+      websockets[0][:path].should eq "/user"
+      websockets[0][:handler].is_a?(Amber::WebSockets::Server::Handler).should be_true
+      websockets[1][:path].should eq "/room"
+      websockets[1][:handler].is_a?(Amber::WebSockets::Server::Handler).should be_true
     end
   end
 end
