a61f64e40ebf846c1d2031e4fc422cffb31bb44b
Isaac Sloan <isaac@isaacsloan.com>
2017-09-07 12:36:51 -0600

    formatting

--- src/amber/server/settings.cr
+++ src/amber/server/settings.cr
@@ -9,23 +9,23 @@ module Amber
     class_getter handler = Pipe::Pipeline.new
     class_getter router = Router::Router.new
     class_property pubsub_adapter : WebSocketAdapter = WebSockets::Adapters::MemoryAdapter
-    class_property port_reuse : Bool 
+    class_property port_reuse : Bool
     class_property log : ::Logger
     class_property env = ENV["AMBER_ENV"]? || "development"
-    class_property process_count : Int32 
-    class_property secret_key_base : String 
-    class_property port : Int32 
+    class_property process_count : Int32
+    class_property secret_key_base : String
+    class_property port : Int32
     class_property name : String
     class_property host : String
     class_property ssl_key_file : String? = nil
     class_property ssl_cert_file : String? = nil
-    class_property redis_url = "" 
-    class_property session : Hash(Symbol, String) 
+    class_property redis_url = ""
+    class_property session : Hash(Symbol, String)
 
-    #loads settings from environment yaml
+    # loads settings from environment yaml
     {{ run("./environment.cr") }}
 
-    #TODO: Remove as ASAP!
+    # TODO: Remove as ASAP!
     class_getter key_generator = Amber::Support::CachingKeyGenerator.new(
       Amber::Support::KeyGenerator.new(@@secret_key_base.to_s, 5)
     )
