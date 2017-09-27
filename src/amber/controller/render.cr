49aede6b132a7fe6872ac91899d960b0f2945e88
Isaac Sloan <isaac@isaacsloan.com>
2017-09-27 00:32:09 -0600

    small cleanup for render macro.

--- src/amber/controller/render.cr
+++ src/amber/controller/render.cr
@@ -11,27 +11,27 @@ module Amber::Controller
     end
 
     macro render(template = nil, partial = nil, layout = true, path = "src/views", folder = __FILE__)
-      {% if template || partial %}
-        {{filename = template || partial}}
-
-        {% if filename.id.split("/").size > 1 %}
-          %content = render_template("#{{{filename}}}", {{path}})
-        {% else %}
-          {% if folder.id.ends_with?(".ecr") %}
-            %content = render_template("#{{{folder.split("/")[-2]}}}/#{{{filename}}}", {{path}})
-          {% else %}
-            %content = render_template("#{{{folder.split("/").last.gsub(/\_controller\.cr|\.cr/, "")}}}/#{{{filename}}}", {{path}})
-          {% end %}
-        {% end %}
-
-        {% if layout && !partial %}
-          content = %content
-          %content = render_template("layouts/#{{{layout.class_name == "StringLiteral" ? layout : LAYOUT}}}", {{path}})
-        {% else %}
-          %content
-        {% end %}
-      {% else %}
+      {% if !(template || partial) %}
         raise "Template or partial required!"
+      {% end %}
+
+      {{filename = template || partial}}
+
+      {% if filename.id.split("/").size > 1 %}
+        %content = render_template("#{{{filename}}}", {{path}})
+      {% else %}
+        {% if folder.id.ends_with?(".ecr") %}
+          %content = render_template("#{{{folder.split("/")[-2]}}}/#{{{filename}}}", {{path}})
+        {% else %}
+          %content = render_template("#{{{folder.split("/").last.gsub(/\_controller\.cr|\.cr/, "")}}}/#{{{filename}}}", {{path}})
+        {% end %}
+      {% end %}
+
+      {% if layout && !partial %}
+        content = %content
+        render_template("layouts/#{{{layout.class_name == "StringLiteral" ? layout : LAYOUT}}}", {{path}})
+      {% else %}
+        %content
       {% end %}
     end
   end
