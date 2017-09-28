b3a21a35c27d8bcb2408fb1757df3f9772567eb9
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2017-09-28 08:06:05 -0500

    Remove -1 on Content-Range

--- src/amber/router/pipe/static.cr
+++ src/amber/router/pipe/static.cr
@@ -135,7 +135,7 @@ module Amber
           env.response.status_code = 206
           env.response.content_length = endb - startb
           env.response.headers["Accept-Ranges"] = "bytes"
-          env.response.headers["Content-Range"] = "bytes #{startb}-#{endb - 1}/#{fileb}" # MUST
+          env.response.headers["Content-Range"] = "bytes #{startb}-#{endb}/#{fileb}" # MUST
 
           if startb > 1024
             skipped = 0
