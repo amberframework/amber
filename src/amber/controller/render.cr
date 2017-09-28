643f163116413a3d66ff62bf256e7c126d57c6bf
Isaac Sloan <isaac@isaacsloan.com>
2017-09-27 20:21:31 -0700

    Render Refactor [Breaking Change] - Also fixes some things (#278)
    * fixed issue with render and made it a bit closer to rails render
    
    * fixed spec
    
    * small cleanup for render macro.
    
    * moved render template under render

--- src/amber/controller/render.cr
+++ src/amber/controller/render.cr
@@ -2,36 +2,36 @@ module Amber::Controller
   module Render
     LAYOUT = "application.slang"
 
+    macro render(template = nil, partial = nil, layout = true, path = "src/views", folder = __FILE__)
+      {% if !(template || partial) %}
+        raise "Template or partial required!"
+      {% end %}
+
+      {{filename = template || partial}}
+
+      {% if filename.id.split("/").size > 1 %}
+        %content = render_template("#{{{filename}}}", {{path}})
+      {% else %}
+        {% if folder.id.ends_with?(".ecr") %}
+          %content = render_template("#{{{folder.split("/")[-2]}}}/#{{{filename}}}", {{path}})
+        {% else %}
+          %content = render_template("#{{{folder.split("/").last.gsub(/\_controller\.cr|\.cr/, "")}}}/#{{{filename}}}", {{path}})
+        {% end %}
+      {% end %}
+
+      {% if layout && !partial %}
+        content = %content
+        render_template("layouts/#{{{layout.class_name == "StringLiteral" ? layout : LAYOUT}}}", {{path}})
+      {% else %}
+        %content
+      {% end %}
+    end
+
     private macro render_template(filename, path = "src/views")
       {% if filename.id.split("/").size > 2 %}
         Kilt.render("{{filename.id}}")
       {% else %}
         Kilt.render("#{{{path}}}/{{filename.id}}")
-      {% end %}
-    end
-
-    macro render(template = nil, partial = nil, layout = true, path = "src/views", folder = __FILE__)
-      {% if template || partial %}
-        {{filename = template || partial}}
-
-        {% if filename.id.split("/").size > 1 %}
-          %content = render_template("#{{{filename}}}", {{path}})
-        {% else %}
-          {% if folder.id.ends_with?(".ecr") %}
-            %content = render_template("#{{{folder.split("/")[-2]}}}/#{{{filename}}}", {{path}})
-          {% else %}
-            %content = render_template("#{{{folder.split("/").last.gsub(/\_controller\.cr|\.cr/, "")}}}/#{{{filename}}}", {{path}})
-          {% end %}
-        {% end %}
-
-        {% if layout && !partial %}
-          content = %content
-          %content = render_template("layouts/#{{{layout.class_name == "StringLiteral" ? layout : LAYOUT}}}", {{path}})
-        {% else %}
-          %content
-        {% end %}
-      {% else %}
-        raise "Template or partial required!"
       {% end %}
     end
   end
