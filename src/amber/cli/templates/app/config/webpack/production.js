aefbf14400e2ddc911bf0d5b1a4e4e38a255b78f
Faustino Aguilar <faustinoaq@gmail.com>
2017-10-11 17:53:11 -0500

    Extract CSS from JS bundle
    It moves all the required *.css modules in entry chunks into a separate CSS file.
    
    - Faster runtime (less code and DOM operations)
    - Styles are no longer inlined into the JS bundle.
    -  If your total stylesheet volume is big, it will be faster because the CSS bundle is loaded in parallel to the JS bundle.

--- src/amber/cli/templates/app/config/webpack/production.js
+++ src/amber/cli/templates/app/config/webpack/production.js
@@ -1,10 +1,13 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./common.js');
+const CssoWebpackPlugin = require('csso-webpack-plugin').default;
 
 module.exports = merge(common, {
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false }
-    })
+    }),
+    new CssoWebpackPlugin(),
   ]
 });
