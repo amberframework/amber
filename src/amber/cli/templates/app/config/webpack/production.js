7c671f92b5aac1a3578a4c41bd31f6c13f549ab1
Faustino Aguilar <faustinoaq@gmail.com>
2017-10-11 19:29:07 -0500

    Remove webpack plugin to minify css

--- src/amber/cli/templates/app/config/webpack/production.js
+++ src/amber/cli/templates/app/config/webpack/production.js
@@ -1,13 +1,11 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./common.js');
-const CssoWebpackPlugin = require('csso-webpack-plugin').default;
 
 module.exports = merge(common, {
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false }
-    }),
-    new CssoWebpackPlugin(),
+    })
   ]
 });
