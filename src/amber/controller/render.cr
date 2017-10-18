1e8a04aed09186254e82831f2c89ace573f87dde
Isaac Sloan <isaac@isaacsloan.com>
2017-10-17 23:33:41 -0600

    methods to set content type and content

--- src/amber/controller/render.cr
+++ src/amber/controller/render.cr
@@ -2,6 +2,24 @@ module Amber::Controller
   module Render
     LAYOUT = "application.slang"
 
+    protected def render_text(body, status_code = 200)
+      context.response.status_code = status_code
+      context.response.content_type = "text/plain"
+      context.content = body
+    end
+
+    protected def render_json(body, status_code = 200)
+      context.response.status_code = status_code
+      context.response.content_type = "text/json"
+      context.content = body
+    end
+
+    protected def render_html(body, status_code = 200)
+      context.response.status_code = status_code
+      context.response.content_type = "text/html"
+      context.content = body
+    end
+
     macro render(template = nil, layout = true, partial = nil, path = "src/views", folder = __FILE__)
       {% if !(template || partial) %}
         raise "Template or partial required!"
@@ -28,11 +46,11 @@ module Amber::Controller
     end
 
     private macro render_template(filename, path = "src/views")
-      {% if filename.id.split("/").size > 2 %}
-        Kilt.render("{{filename.id}}")
-      {% else %}
-        Kilt.render("#{{{path}}}/{{filename.id}}")
-      {% end %}
-    end
+    {% if filename.id.split("/").size > 2 %}
+      Kilt.render("{{filename.id}}")
+    {% else %}
+      Kilt.render("#{{{path}}}/{{filename.id}}")
+    {% end %}
   end
+end
 end
