c0704513ae702e30d59eab17527f3bb88d559c23
Isaac Sloan <isaac@isaacsloan.com>
2017-10-24 00:51:03 -0600

    modified renderer to allow for namespacing

--- src/amber/controller/render.cr
+++ src/amber/controller/render.cr
@@ -7,28 +7,29 @@ module Amber::Controller
         raise "Template or partial required!"
       {% end %}
 
-      {{filename = template || partial}}
+      {{ filename = template || partial }}
 
       {% if filename.id.split("/").size > 1 %}
         %content = render_template("#{{{filename}}}", {{path}})
       {% else %}
+        {{ short_path = folder.gsub(/^.+?(?:controllers|views)\//, "") }}
         {% if folder.id.ends_with?(".ecr") %}
-          %content = render_template("#{{{folder.split("/")[-2]}}}/#{{{filename}}}", {{path}})
+          %content = render_template("#{{{path}}}/#{{{short_path.gsub(/\/[^\.\/]+\.ecr/, "")}}}/#{{{filename}}}")
         {% else %}
-          %content = render_template("#{{{folder.split("/").last.gsub(/\_controller\.cr|\.cr/, "")}}}/#{{{filename}}}", {{path}})
+          %content = render_template("#{{{path}}}/#{{{short_path.gsub(/\_controller\.cr|\.cr/, "")}}}/#{{{filename}}}")
         {% end %}
       {% end %}
 
       {% if layout && !partial %}
         content = %content
-        render_template("layouts/#{{{layout.class_name == "StringLiteral" ? layout : LAYOUT}}}", {{path}})
+        render_template("#{{{path}}}/layouts/#{{{layout.class_name == "StringLiteral" ? layout : LAYOUT}}}")
       {% else %}
         %content
       {% end %}
     end
 
     private macro render_template(filename, path = "src/views")
-      {% if filename.id.split("/").size > 2 %}
+      {% if filename.id.split("/").size > 3 %}
         Kilt.render("{{filename.id}}")
       {% else %}
         Kilt.render("#{{{path}}}/{{filename.id}}")
@@ -36,3 +37,4 @@ module Amber::Controller
     end
   end
 end
+
