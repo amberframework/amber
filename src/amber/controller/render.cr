69dcc44245f00e55a03532ad5b7c472483071aa2
Isaac Sloan <isaac@isaacsloan.com>
2017-10-25 14:18:34 -0600

    Fixed an issue I found with render if we use namespaces. (#328)
    * modified renderer to allow for namespacing
    
    * fixup
    
    * formatting
    
    * commented

--- src/amber/controller/render.cr
+++ src/amber/controller/render.cr
@@ -7,21 +7,24 @@ module Amber::Controller
         raise "Template or partial required!"
       {% end %}
 
-      {{filename = template || partial}}
-
+      {{ filename = template || partial }}
+       
+      # Render Template and return content
       {% if filename.id.split("/").size > 1 %}
         %content = render_template("#{{{filename}}}", {{path}})
       {% else %}
+        {{ short_path = folder.gsub(/^.+?(?:controllers|views)\//, "") }}
         {% if folder.id.ends_with?(".ecr") %}
-          %content = render_template("#{{{folder.split("/")[-2]}}}/#{{{filename}}}", {{path}})
+          %content = render_template("#{{{path}}}/#{{{short_path.gsub(/\/[^\.\/]+\.ecr/, "")}}}/#{{{filename}}}")
         {% else %}
-          %content = render_template("#{{{folder.split("/").last.gsub(/\_controller\.cr|\.cr/, "")}}}/#{{{filename}}}", {{path}})
+          %content = render_template("#{{{path}}}/#{{{short_path.gsub(/\_controller\.cr|\.cr/, "")}}}/#{{{filename}}}")
         {% end %}
       {% end %}
-
+      
+      # Render Layout
       {% if layout && !partial %}
         content = %content
-        render_template("layouts/#{{{layout.class_name == "StringLiteral" ? layout : LAYOUT}}}", {{path}})
+        render_template("#{{{path}}}/layouts/#{{{layout.class_name == "StringLiteral" ? layout : LAYOUT}}}")
       {% else %}
         %content
       {% end %}
