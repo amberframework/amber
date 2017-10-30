3353eb87be79057e0a9bac76f3ddd725af94097a
Mark Siemers <mark.siemers@gmail.com>
2017-10-30 09:54:49 -0700

    Name databases consistently for pg, mysql, sqlite

--- src/amber/cli/templates/app.cr
+++ src/amber/cli/templates/app.cr
@@ -1,9 +1,11 @@
 module Amber::CLI
   class App < Teeplate::FileTree
     directory "#{__DIR__}/app"
+    getter database_name_base
 
     @name : String
     @database : String
+    @database_name_base : String
     @language : String
     @model : String
     @db_url : String
@@ -12,10 +14,16 @@ module Amber::CLI
     def initialize(@name, @database = "pg", @language = "slang", @model = "granite")
       @db_url = ""
       @wait_for = ""
+      @database_name_base = generate_database_name_base
     end
 
     def filter(entries)
       entries.reject { |entry| entry.path.includes?("src/views") && !entry.path.includes?("#{@language}") }
     end
+
+    private def generate_database_name_base
+      @name.gsub('-','_')
+    end
+
   end
 end
