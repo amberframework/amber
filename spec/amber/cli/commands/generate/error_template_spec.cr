c67c7a47d4fdf736914e03659eea17bcf894282c
Mark Siemers <mark.siemers@gmail.com>
2017-10-31 16:12:46 -0700

    Add tests for error controller spec file and switch to using 'class_name' (#294)

--- spec/amber/cli/commands/generate/error_template_spec.cr
+++ spec/amber/cli/commands/generate/error_template_spec.cr
@@ -20,11 +20,37 @@ module Amber::CLI
             File.exists?("./spec/controllers/error_controller_spec.cr").should be_true
           end
 
+          it "generates a controller spec file with correct class name" do
+            expected = "ErrorController"
+            File.read("./spec/controllers/error_controller_spec.cr").should contain expected
+          end
+
           it "generates view files" do
             ["forbidden", "not_found", "internal_server_error"].each do |view|
               File.exists?("./src/views/error/#{view}.slang").should be_true
             end
           end
+
+          {% if flag?(:run_builds) || flag?(:run_test_app_builds)%}
+
+            describe "error_controller_spec.cr" do
+              `shards build`
+              result = `crystal spec ./spec/controllers/error_controller_spec.cr`
+              
+              it "can be executed" do
+                result.should contain "Finished in"
+              end
+
+              it "has no errors" do
+                result.should_not contain "Error in line"
+              end
+
+              it "has no failures" do
+                result.should_not contain "Failures"
+              end
+            end
+
+          {% end %}
 
         end
       end
