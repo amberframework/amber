d459d7e65d436d92f393722b3c8ab2397ccaffce
Mark <mark.siemers@gmail.com>
2017-10-31 06:47:48 -0700

    Fix postgresql database naming convention (#332)
    * Add test for generating and app with postgres
    
    * Add test for database url in environment yaml files
    
    * Add tests for database urls and names generated for docker-compose.yml
    
    * Replace '-' with '_' for postgresql database names
    
    * Switch default postgresql username to postgres in database.yml
    
    * Name databases consistently for pg, mysql, sqlite
    
    * Test Amber::CLI::App directly for database naming
    
    * Use 'database_name_base' property in all parts of docker-compose.yml
    
    
    Former-commit-id: 69716c4e3658e049b0a822b28d1916f7943c1b5a

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -4,7 +4,7 @@ ENV["AMBER_ENV"] = "test"
 TEST_PATH     = "spec/support/sample"
 PUBLIC_PATH   = TEST_PATH + "/public"
 VIEWS_PATH    = TEST_PATH + "/views"
-TEST_APP_NAME = "test_app"
+TEST_APP_NAME = "sample-app"
 TESTING_APP   = "./tmp/#{TEST_APP_NAME}"
 APP_TPL_PATH  = "./src/amber/cli/templates/app"
 CURRENT_DIR   = Dir.current
@@ -30,6 +30,15 @@ module Amber::CLI::Spec
     gen_dirs.map { |dir| dir[(app.size + 1)..-1] }
   end
 
+  def db_name(db_url : String) : String
+    db_name(URI.parse(db_url))
+  end
+
+  def db_name(db_uri : URI) : String
+    path = db_uri.path
+    path ? path.split('/').last : ""
+  end
+
   def db_yml
     YAML.parse(File.read("#{TESTING_APP}/config/database.yml"))
   end
@@ -40,6 +49,26 @@ module Amber::CLI::Spec
 
   def shard_yml
     YAML.parse(File.read("#{TESTING_APP}/shard.yml"))
+  end
+
+  def environment_yml(environment : String)
+    YAML.parse(File.read("#{TESTING_APP}/config/environments/#{environment}.yml"))
+  end
+
+  def development_yml
+    environment_yml("development")
+  end
+
+  def production_yml
+    environment_yml("production")
+  end
+
+  def test_yml
+    environment_yml("test")
+  end
+
+  def docker_compose_yml
+    YAML.parse(File.read("#{TESTING_APP}/docker-compose.yml"))
   end
 
   def prepare_yaml(path)
