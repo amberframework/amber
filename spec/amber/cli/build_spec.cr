35bcbd7fbac4a04664a5ffa95fa48ca9711f71d9
Mark Siemers <mark.siemers@gmail.com>
2017-11-02 11:54:47 -0700

    Move build test to its own file and test more aspects of build

--- spec/amber/cli/build_spec.cr
+++ spec/amber/cli/build_spec.cr
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 {% if flag?(:run_build_tests) %}
 require "../../spec_helper"
 require "../../../support/helpers/cli_helper"
@@ -54,3 +55,60 @@ module Amber::CLI
   end
 end
 {% end %}
+||||||| merged common ancestors
+=======
+{% if flag?(:run_build_tests) %}
+require "../../spec_helper"
+require "../../../support/helpers/cli_helper"
+
+include CLIHelper
+
+module Amber::CLI
+  begin
+    describe "building a generated app" do
+      ENV["AMBER_ENV"] = "test"
+      MainCommand.run ["new", TESTING_APP]
+      Dir.cd(TESTING_APP)
+
+      options = ["user:reference", "name:string", "body:text", "age:integer", "published:bool"]
+      MainCommand.run ["generate", "auth", "User"] | (options - ["user:reference"])
+      MainCommand.run ["generate", "scaffold", "Animal"] | options
+      MainCommand.run ["generate", "scaffold", "Post"] | options
+      MainCommand.run ["generate", "scaffold", "PostComment"] | (options + ["post:reference"])
+      MainCommand.run ["generate", "model", "Bat"] | options
+      MainCommand.run ["generate", "migration", "Crocodile"] | options
+      MainCommand.run ["generate", "mailer", "Dinosaur"] | options
+      MainCommand.run ["generate", "socket", "Eagle"] | options
+      MainCommand.run ["generate", "channel", "Falcon"] | options
+
+      prepare_yaml(Dir.current)
+
+      build_result = `shards build`
+      db_result = `amber db drop create migrate`
+
+      it `generates a binary` do
+        File.exists?("bin/#{TEST_APP_NAME}").should be_true
+      end
+
+      context "crystal spec" do
+        spec_result = `crystal spec`
+
+        it "can be executed" do
+          spec_result.should contain "Finished in"
+        end
+
+        it "has no errors" do
+          spec_result.should_not contain "Error in line"
+        end
+
+        it "has no failures" do
+          spec_result.should_not contain "Failures"
+        end
+      end
+    end
+  ensure
+    cleanup
+  end
+end
+{% end %}
+>>>>>>> Move build test to its own file and test more aspects of build
