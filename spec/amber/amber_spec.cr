44ce618f8bb2802670aa4051c8c21ed7e612cdec
Isaac Sloan <isaac@isaacsloan.com>
2017-11-07 16:03:43 -0700

    added requested test change

--- spec/amber/amber_spec.cr
+++ spec/amber/amber_spec.cr
@@ -17,24 +17,6 @@ describe Amber do
 
   describe Amber::Server do
     describe ".configure" do
-      it "loads environment settings from test.yml" do
-        settings = Amber::Settings.new
-
-        settings.name.should eq "amber_test_app"
-        settings.port_reuse.should eq true
-        settings.redis_url.should eq "#{ENV["REDIS_URL"]? || "redis://localhost:6379"}"
-        settings.port.should eq 3000
-        settings.color.should eq true
-        settings.secret_key_base.should eq "mV6kTmG3k1yVFh-fPYpugSn0wbZveDvrvfQuv88DPF8"
-        expected_session = {:key => "amber.session", :store => :signed_cookie, :expires => 0}
-        settings.session.should eq expected_session
-        expected_secrets = {
-          description: "Store your test secrets credentials and settings here.",
-          database:    "mysql://root@localhost:3306/amber_test_app_test",
-        }
-        settings.secrets.should eq expected_secrets
-      end
-
       it "overrides enviroment settings" do
         Amber::Server.instance.settings = Amber::Settings.new
         Amber::Server.configure do |server|
