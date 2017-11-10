4c71900414441f5853f9bab879bb16f0af8d98fb
Isaac Sloan <isaac@isaacsloan.com>
2017-11-09 23:25:50 -0700

    this now behaves as close to a console as we can make it

--- src/amber/cli/commands/exec.cr
+++ src/amber/cli/commands/exec.cr
@@ -2,7 +2,7 @@ require "cli"
 
 module Amber::CLI
   class MainCommand < ::Cli::Supercommand
-    command "e", aliased: "exec"
+    command "ex", aliased: "exec"
 
     class Exec < ::Cli::Command
       command_name "exec"
@@ -10,7 +10,7 @@ module Amber::CLI
       class Options
         arg "code", desc: "Crystal code or .cr file to execute within the application scope", required: false, default: nil
         string ["-e", "--editor"], desc: "Prefered editor: [vim, nano, pico, etc], only used when no code or .cr file is specified", default: "vim"
-        bool ["-p", "--persist"], desc: "Persist the code across invocations of 'amber exec'"
+        string ["-b", "--back"], desc: "Runs prevous command files 'amber exec'", default: "0"
       end
 
       class Help
@@ -21,15 +21,21 @@ module Amber::CLI
         result = ""
         if args.size > 0 && args.code
           if args.code.ends_with?(".cr") && File.exists?(args.code)
+            system("#{options.editor} #{args.code}")
             result = `crystal eval 'require "../config/*"; require "./#{args.code}"'`
           else
             result = `crystal eval 'require "../config/*"; puts (#{args.code}).inspect'`
           end
         else
-          system("#{options.editor} .amber_exec.cr")
-          if File.exists?(".amber_exec.cr")
-            result = `crystal eval 'require "../config/*"; require "./.amber_exec.cr"'`
-            File.delete(".amber_exec.cr") unless options.persist?
+          Dir.mkdir("tmp") unless Dir.exists?("tmp")
+          filename = "./tmp/console_#{Time.now.epoch}.cr"
+          if options.back.to_i(strict: false) > 0
+            old_filename = Dir.glob("./tmp/console_*.cr").reverse[options.back.to_i(strict: false) - 1]
+            system("cp #{old_filename} #{filename}")
+          end
+          system("#{options.editor} #{filename}")
+          if File.exists?(filename)
+            result = `crystal eval 'require "../config/*"; require "#{filename}"'`
           end
         end
         if result.includes?("Error in line 1:") &&
