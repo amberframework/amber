1457fdad532c2ca137a5d8b6ec4b8f1988552517
Isaac Sloan <isaac@isaacsloan.com>
2017-11-10 10:12:24 -0700

    cleaned up code a bit and copied existing file to new temp file for edits

--- src/amber/cli/commands/exec.cr
+++ src/amber/cli/commands/exec.cr
@@ -2,15 +2,17 @@ require "cli"
 
 module Amber::CLI
   class MainCommand < ::Cli::Supercommand
-    command "ex", aliased: "exec"
+    command "x", aliased: "exec"
 
     class Exec < ::Cli::Command
       command_name "exec"
+      @filename = "./tmp/console_#{Time.now.epoch}.cr"
+      @result : String? = nil
 
       class Options
-        arg "code", desc: "Crystal code or .cr file to execute within the application scope", required: false, default: nil
+        arg "code", desc: "Crystal code or .cr file to execute within the application scope", default: ""
         string ["-e", "--editor"], desc: "Prefered editor: [vim, nano, pico, etc], only used when no code or .cr file is specified", default: "vim"
-        string ["-b", "--back"], desc: "Runs prevous command files 'amber exec'", default: "0"
+        string ["-b", "--back"], desc: "Runs prevous command files: 'amber exec -b [times_ago]'", default: "0"
       end
 
       class Help
@@ -18,31 +20,30 @@ module Amber::CLI
       end
 
       def run
-        result = ""
-        if args.size > 0 && args.code
-          if args.code.ends_with?(".cr") && File.exists?(args.code)
-            result = `crystal eval 'require "../config/*"; require "./#{args.code}"'`
-          else
-            result = `crystal eval 'require "../config/*"; puts (#{args.code}).inspect'`
-          end
+        unless args.code.blank? || File.exists?(args.code)
+          @result = `crystal eval 'require "../config/*"; puts (#{args.code}).inspect'`
         else
-          Dir.mkdir("tmp") unless Dir.exists?("tmp")
-          filename = "./tmp/console_#{Time.now.epoch}.cr"
-          if options.back.to_i(strict: false) > 0
-            old_filename = Dir.glob("./tmp/console_*.cr").reverse[options.back.to_i(strict: false) - 1]
-            system("cp #{old_filename} #{filename}")
-          end
-          system("#{options.editor} #{filename}")
-          if File.exists?(filename)
-            result = `crystal eval 'require "../config/*"; require "#{filename}"'`
-          end
+          prepare_file
+          system("#{options.editor} #{@filename}")
+          @result = `crystal eval 'require "../config/*"; require "#{@filename}"'` if File.exists?(@filename)
         end
-        if result.includes?("Error in line 1:") &&
-           result.includes?("while requiring \"../config/*\": can't find file '../config/*' relative to '.'")
-          result = "error: 'amber exec' can only be used from the root of a valid amber project"
+
+        if @result && @result.not_nil!.includes?("while requiring \"../config/*\": can't find file '../config/*' relative to '.'")
+          @result = "Error: 'amber exec' can only be used from the root of a valid amber project"
         end
-        puts result
-        result
+        puts @result
+        @result
+      end
+
+      def prepare_file
+        _filename = if File.exists?(args.code)
+                      args.code
+                    elsif options.back.to_i(strict: false) > 0 
+                      Dir.glob("./tmp/console_*.cr").reverse[options.back.to_i(strict: false) - 1]?
+                    end
+
+        system("cp #{_filename} #{@filename}") if _filename
+        @filename
       end
     end
   end
