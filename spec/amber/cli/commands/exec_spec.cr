2b1bb15a1998c6eb0e0fb67b3b56dfcbce44fdf0
Isaac Sloan <isaac@isaacsloan.com>
2017-11-10 23:48:21 -0700

    sorted ls results

--- spec/amber/cli/commands/exec_spec.cr
+++ spec/amber/cli/commands/exec_spec.cr
@@ -14,28 +14,28 @@ module Amber::CLI
       it "executes one-liners from the first command-line argument" do
         expected_result = "[:a, :b, :c, :d]\n"
         MainCommand.run(["exec", "[:a, :b, :c] + [:d]"])
-        logs = `ls tmp/*_console_result.log`.strip.split(/\s/)
+        logs = `ls tmp/*_console_result.log`.strip.split(/\s/).sort
         File.read(logs.last?.to_s).should eq expected_result
       end
 
       it "executes a .cr file from the first command-line argument" do
         File.write "amber_exec_spec_test.cr", "puts([:a] + [:b])"
         MainCommand.run(["exec", "amber_exec_spec_test.cr", "-e", "tail"])
-        logs = `ls tmp/*_console_result.log`.strip.split(/\s/)
+        logs = `ls tmp/*_console_result.log`.strip.split(/\s/).sort
         File.read(logs.last?.to_s).should eq "[:a, :b]\n"
         File.delete("amber_exec_spec_test.cr")
       end
 
       it "opens editor and executes .cr file on close" do
         MainCommand.run(["exec", "-e", "echo 'puts 1000' > "])
-        logs = `ls tmp/*_console_result.log`.strip.split(/\s/)
+        logs = `ls tmp/*_console_result.log`.strip.split(/\s/).sort
         File.read(logs.last?.to_s).should eq "1000\n"
       end
 
       it "copies previous run into new file for editing and runs it returning results" do
         MainCommand.run(["exec", "1337"])
         MainCommand.run(["exec", "-e", "tail", "-b", "1"])
-        logs = `ls tmp/*_console_result.log`.strip.split(/\s/)
+        logs = `ls tmp/*_console_result.log`.strip.split(/\s/).sort
         File.read(logs.last?.to_s).should eq "1337\n"
       end
 
@@ -46,7 +46,7 @@ module Amber::CLI
       it "complains if not in the root of a project" do
         expected_result = "Error: 'amber exec' can only be used from the root of a valid amber project"
         MainCommand.run(["exec", ":hello"])
-        logs = `ls tmp/*_console_result.log`.strip.split(/\s/)
+        logs = `ls tmp/*_console_result.log`.strip.split(/\s/).sort
         File.read(logs.last?.to_s).should eq expected_result
       end
 
