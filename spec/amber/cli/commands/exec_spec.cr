491c71e953d0caf346ed5608898fb3925d33aa98
Isaac Sloan <isaac@isaacsloan.com>
2017-11-10 22:31:51 -0700

    formatted and fixed tests probably

--- spec/amber/cli/commands/exec_spec.cr
+++ spec/amber/cli/commands/exec_spec.cr
@@ -1,3 +1,4 @@
+{% if flag?(:run_build_tests) %}
 require "../../../spec_helper"
 require "../../../support/helpers/cli_helper"
 
@@ -50,3 +51,4 @@ module Amber::CLI
     cleanup
   end
 end
+{% end %}
