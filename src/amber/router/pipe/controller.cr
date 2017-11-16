a0ea36156b3d708c1ce41e6bb54da11199773791
Isaac Sloan <isaac@isaacsloan.com>
2017-11-16 15:28:00 -0700

    Added Amber::Pipe::Last to replace current proc. (#396)
    * added last pipe and made small change to pipeline handler. Tested on app. Worked first time.
    
    * Update controller.cr
    
    
    Former-commit-id: ecad9d68326142e7e08b9ce536a86eb6781f3123

---
+++ src/amber/router/pipe/controller.cr
@@ -0,0 +1,12 @@
+module Amber
+  module Pipe
+    # This is the default last pipe which is inserted automatically 
+    # in order to call the controller action.
+
+    class Last < Base
+      def call(context : HTTP::Server::Context)
+        context.process_request
+      end
+    end
+  end
+end
