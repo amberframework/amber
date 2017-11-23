5bf80c2ba897f518b61a72acae4ea547f50e1842
Elias Perez <eliasjpr@gmail.com>
2017-11-23 11:55:37 -0500

    Parrallelize travis

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -11,6 +11,7 @@ CURRENT_DIR       = Dir.current
 
 Amber.environment_path = "./spec/support/config/"
 Amber.env=(ENV["AMBER_ENV"])
+Amber::Server.settings.redis_url = ENV["REDIS_URL"]? || Amber::Server.settings.redis_url
 
 require "http"
 require "spec"
