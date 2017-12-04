caebcd6dead71628aaac21263152483ca2c06502
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2017-12-04 13:23:51 -0500

    Provide attributes for git config

--- src/amber/cli/templates/app.cr
+++ src/amber/cli/templates/app.cr
@@ -1,5 +1,7 @@
 module Amber::CLI
   class App < Teeplate::FileTree
+    WHICH_GIT_COMMAND = "which git >/dev/null"
+
     directory "#{__DIR__}/app"
     getter database_name_base
 
@@ -10,11 +12,17 @@ module Amber::CLI
     @model : String
     @db_url : String
     @wait_for : String
+    @author : String
+    @email : String
+    @github_name : String
 
     def initialize(@name, @database = "pg", @language = "slang", @model = "granite")
       @db_url = ""
       @wait_for = ""
       @database_name_base = generate_database_name_base
+      @author = fetch_author
+      @email = fetch_email
+      @github_name = fetch_github_name
     end
 
     def filter(entries)
@@ -23,6 +31,23 @@ module Amber::CLI
 
     private def generate_database_name_base
       @name.gsub('-', '_')
+    end
+
+    def fetch_author
+      return "[your-name-here]" unless system(WHICH_GIT_COMMAND)
+      `git config --get user.name`.strip
+    end
+
+    def fetch_email
+      return "[your-email-here]" unless system(WHICH_GIT_COMMAND)
+      `git config --get user.email`.strip
+    end
+
+    def fetch_github_name
+      default = "[your-github-name]"
+      return default unless system(WHICH_GIT_COMMAND)
+      github_user = `git config --get github.user`.strip
+      github_user.empty? ? default : github_user
     end
   end
 end
