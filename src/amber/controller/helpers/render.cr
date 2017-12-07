8b2f0975e81d57c9cd880b2e42e8e31d0b8c6d0a
Damian Hamill <damianham@gmail.com>
2017-12-07 15:28:04 +0700

    support modules of code with controller and views in the same folder

--- src/amber/controller/helpers/render.cr
+++ src/amber/controller/helpers/render.cr
@@ -2,6 +2,33 @@ module Amber::Controller::Helpers
   module Render
     LAYOUT = "application.slang"
 
+    macro render_module(template = nil, layout = true, partial = nil, path = "src/views", folder = __DIR__)
+      {% if !(template || partial) %}
+        raise "Template or partial required!"
+      {% end %}
+
+      {{ filename = template || partial }}
+
+      {% if filename.id.split("/").size > 1 %}
+        %content = render_template("#{{{filename}}}", {{path}})
+      {% else %}
+        {{ short_path = folder.gsub(/^.+?(?:controllers|views)\//, "") }}
+        {% if folder.id.ends_with?(".ecr") %}
+          %content = render_template("#{{{short_path.gsub(/\/[^\.\/]+\.ecr/, "")}}}/#{{{filename}}}")
+        {% else %}
+          %content = render_template("#{{{short_path.gsub(/\_controller\.cr|\.cr/, "")}}}/#{{{filename}}}")
+        {% end %}
+      {% end %}
+
+      # Render Layout
+      {% if layout && !partial %}
+        content = %content
+        render_template("#{{{path}}}/layouts/#{{{layout.class_name == "StringLiteral" ? layout : LAYOUT}}}")
+      {% else %}
+        %content
+      {% end %}
+    end
+
     macro render(template = nil, layout = true, partial = nil, path = "src/views", folder = __FILE__)
       {% if !(template || partial) %}
         raise "Template or partial required!"
