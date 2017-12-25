3c1ee7d838a6da2626f5fc080ad2d100f4c84120
Damian Hamill <damianham@gmail.com>
2017-12-25 20:22:54 +0700

    handle the case where there is no accept header (#462)

--- src/amber/router/context.cr
+++ src/amber/router/context.cr
@@ -70,14 +70,20 @@ class HTTP::Server::Context
   {% end %}
 
   def format
-    content_type = request.headers[FORMAT_HEADER].split(",").first
-    type = if content_type.includes?(";")
-             content_type.split(";").first
-           else
-             content_type
-           end
+    content_type = request.headers[FORMAT_HEADER]?
 
-    Amber::Support::MimeTypes.format(type)
+    if content_type
+      content_type = content_type.split(",").first
+      type = if content_type.includes?(";")
+        content_type.split(";").first
+      else
+        content_type
+      end
+
+      Amber::Support::MimeTypes.format(type)
+    else
+      Amber::Support::MimeTypes.default
+    end
   end
 
   def port
