bc3e7db216ab459166fe1c7b1a265178ba63dcf5
Rostyslav Diachok <infernalmaster1@gmail.com>
2018-01-04 13:51:37 +0200

    mask token to prevent breach attacks

--- src/amber/router/pipe/csrf.cr
+++ src/amber/router/pipe/csrf.cr
@@ -9,13 +9,12 @@ module Amber
       HEADER_KEY    = "X-CSRF-TOKEN"
       PARAM_KEY     = "_csrf"
       CSRF_KEY      = "csrf.token"
+      TOKEN_LENGTH = 32
 
-      def initialize(@refresh_token : Bool = true)
-      end
+      class_property token_strategry = PersistantToken
 
       def call(context : HTTP::Server::Context)
-        if valid_http_method?(context) || valid_token?(context)
-          context.session.delete(CSRF_KEY) if @refresh_token
+        if valid_http_method?(context) || self.class.token_strategry.valid_token?(context)
           call_next(context)
         else
           raise Amber::Exceptions::Forbidden.new("CSRF check failed.")
@@ -26,15 +25,8 @@ module Amber
         !CHECK_METHODS.includes?(context.request.method)
       end
 
-      def valid_token?(context)
-        request_token = context.params[PARAM_KEY]? || context.request.headers[HEADER_KEY]?
-        session_token = self.class.token(context).to_s
-
-        request_token && Crypto::Subtle.constant_time_compare(request_token, session_token)
-      end
-
       def self.token(context)
-        context.session[CSRF_KEY] ||= Random::Secure.urlsafe_base64(32)
+        token_strategry.token(context)
       end
 
       def self.tag(context)
@@ -42,7 +34,80 @@ module Amber
       end
 
       def self.metatag(context)
-        %Q(<meta name="#{HEADER_KEY}" content="#{token(context)}" />)
+        %Q(<meta name="#{PARAM_KEY}" content="#{token(context)}" />)
+      end
+
+      module BaseToken
+        def request_token(context)
+          context.params[PARAM_KEY]? || context.request.headers[HEADER_KEY]?
+        end
+
+        def real_session_token(context) : String
+          (context.session[CSRF_KEY] ||= Random::Secure.urlsafe_base64(TOKEN_LENGTH)).to_s
+        end
+      end
+
+      module RefreshableToken
+        extend self
+        extend BaseToken
+
+        def token(context) : String
+          real_session_token(context)
+        end
+
+        def valid_token?(context)
+          request_token = request_token(context)
+
+          request_token &&
+            Crypto::Subtle.constant_time_compare(request_token, token(context)) &&
+            context.session.delete(CSRF_KEY)
+        end
+      end
+
+      module PersistantToken
+        extend self
+        extend BaseToken
+
+        def valid_token?(context)
+          request_token = request_token(context)
+          return false if request_token.nil?
+          decoded_request_token = Base64.decode_string(request_token)
+          return false if decoded_request_token.bytesize != 2 * TOKEN_LENGTH
+          unmaseked_request_token = TokenOperations.unmask(decoded_request_token)
+          decoded_session_token = Base64.decode_string(real_session_token(context))
+
+          Crypto::Subtle.constant_time_compare(unmaseked_request_token, decoded_session_token)
+        end
+
+        def token(context) : String
+          unmask_token = Base64.decode_string(real_session_token(context))
+          masked_token = TokenOperations.mask(unmask_token)
+          Base64.urlsafe_encode(masked_token)
+        end
+
+        module TokenOperations
+          extend self
+
+          # Creates a masked version of the authenticity token that varies
+          # on each request. The masking is used to mitigate SSL attacks
+          # like BREACH.
+          def mask(unmasked_token)
+            one_time_pad = Bytes.new(TOKEN_LENGTH).tap { |buf| Random::Secure.random_bytes(buf) }
+            encrypted_csrf_token = xor_byte_strings(one_time_pad, unmasked_token.bytes)
+            "#{String.new(one_time_pad)}#{encrypted_csrf_token}"
+          end
+
+          def unmask(masked_token)
+            one_time_pad = masked_token.bytes[0...TOKEN_LENGTH]
+            encrypted_csrf_token = masked_token.bytes[TOKEN_LENGTH..-1]
+            xor_byte_strings(one_time_pad, encrypted_csrf_token)
+          end
+
+          def xor_byte_strings(s1, s2) : String
+            s1.each_with_index { |c1, i| s2[i] ^= c1 }
+            String.new(s2.to_unsafe, TOKEN_LENGTH)
+          end
+        end
       end
     end
   end
