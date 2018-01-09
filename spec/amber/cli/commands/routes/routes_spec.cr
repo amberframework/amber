4de56a947bfaf6f7b34d96ea8e90a8205cb99347
Mark Siemers <mark.siemers@gmail.com>
2018-01-09 08:46:33 -0800

    Wrap 'amber routes' tests for default routes in describe block

--- spec/amber/cli/commands/routes/routes_spec.cr
+++ spec/amber/cli/commands/routes/routes_spec.cr
@@ -10,38 +10,42 @@ module Amber::CLI
   extend Helpers
   describe "amber routes" do
     begin
+      ENV["CRYSTAL_CLI_ENV"] = "test"
       context "in an `amber new` app with default options" do
         cleanup
         scaffold_app(TESTING_APP)
         output = ""
-        ENV["CRYSTAL_CLI_ENV"] = "test"
-        MainCommand.run %w(routes) { |cmd| output = cmd.out.gets_to_end }
-        output_lines = route_table_rows(output)
 
-        it "outputs the correct headers" do
-          headers = %w(Verb Controller Action Pipeline Scope) << "URI Pattern"
-          headers.each do |header|
-            output.should contain header
+        describe "with the default routes" do
+
+          MainCommand.run %w(routes) { |cmd| output = cmd.out.gets_to_end }
+          output_lines = route_table_rows(output)
+
+          it "outputs the correct headers" do
+            headers = %w(Verb Controller Action Pipeline Scope) << "URI Pattern"
+            headers.each do |header|
+              output.should contain header
+            end
           end
-        end
 
-        it "outputs the static file handler" do
-          expected = "Amber::Controller::Static"
-          output.should contain expected
-          line = output_lines.find(""){ |line| line.includes? expected }
-          expectations = %w(get Amber::Controller::Static index static /*)
-          expectations.each do |expectation|
-            line.should contain expectation
+          it "outputs the static file handler" do
+            expected = "Amber::Controller::Static"
+            output.should contain expected
+            line = output_lines.find(""){ |line| line.includes? expected }
+            expectations = %w(get Amber::Controller::Static index static /*)
+            expectations.each do |expectation|
+              line.should contain expectation
+            end
           end
-        end
 
-        it "outputs the HomeController index action" do
-          expected = "HomeController"
-          output.should contain expected
-          line = output_lines.find(""){ |line| line.includes? expected }
-          expectations = %w(get HomeController index web /)
-          expectations.each do |expectation|
-            line.should contain expectation
+          it "outputs the HomeController index action" do
+            expected = "HomeController"
+            output.should contain expected
+            line = output_lines.find(""){ |line| line.includes? expected }
+            expectations = %w(get HomeController index web /)
+            expectations.each do |expectation|
+              line.should contain expectation
+            end
           end
         end
 
