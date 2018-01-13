2cfb11d82e397e25e1b32bddbfab8867744675d2
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2018-01-13 09:47:57 -0500

    Indent progname

--- src/amber/environment/logger.cr
+++ src/amber/environment/logger.cr
@@ -4,12 +4,12 @@ require "colorize"
 module Amber::Environment
   class Logger < ::Logger
     def puts(message, progname = progname, color = :magenta)
-      log(self.level, message, "#{progname} |".colorize(color))
+      log(self.level, message, ("#{progname}".ljust(10) + " |").colorize(color))
     end
 
     {% for name in Severity.constants %}
       def {{name.id.downcase}}(message, color = :light_cyan)
-        log(Severity::{{name.id}}, message, "#{progname} |".colorize(color))
+        log(Severity::{{name.id}}, message, ("#{progname}".ljust(10) + " |").colorize(color))
       end
     {% end %}
   end
