21699d9c3775eb3c89b77e3f7560ccc36219f052
Davor Ocelic <docelic@spinlocksolutions.com>
2018-01-21 22:42:30 +0100

    Pipe::ClientIp - saves client IP to Amber context
    - Add Amber::Pipe::ClientIp
    - It looks into configured header names (default: "X-Forwarded-For"),
      finds the first header that exists, and takes its last value
    - Saves the value into context.client_ip as Socket::IPAddress(addr, 0)
    
    - Covered by specs
    
    - Along the way, sort in alphabetic order methods on the controller
      that are delegated to context for easier lookup and modification

---
+++ spec/amber/router/pipe/client_ip_spec.cr
@@ -0,0 +1,47 @@
+require "../../../../spec_helper"
+
+module Amber
+  module Pipe
+    describe ClientIp do
+      context "Gets client IP from headers" do
+
+        Amber::Server.router.draw :web do
+          get "/client_ip_address", HelloController, :client_ip_address
+        end
+
+        it "gets last client IP from default header" do
+          pipeline = Pipeline.new
+          pipeline.build :web do
+            plug ClientIp.new
+          end
+          pipeline.prepare_pipelines
+
+          ip_address1 = "102.168.35.88"
+          ip_address2 = "102.168.35.99"
+          headers = HTTP::Headers.new
+          headers["X-Forwarded-For"] = [ip_address1, ip_address2]
+          request = HTTP::Request.new("GET", "/client_ip_address", headers)
+          response = create_request_and_return_io(pipeline, request)
+          response.body.should contain ip_address2
+        end
+
+        it "gets client IP from first custom header found" do
+          pipeline = Pipeline.new
+          pipeline.build :web do
+            plug ClientIp.new(["X-Forwarded", "X-Client-IP"])
+          end
+          pipeline.prepare_pipelines
+
+          ip_address1 = "102.168.35.88"
+          ip_address2 = "102.168.35.99"
+          headers = HTTP::Headers.new
+          headers["X-Forwarded-For"] = ip_address1
+          headers["X-Client-IP"] = ip_address2
+          request = HTTP::Request.new("GET", "/client_ip_address", headers)
+          response = create_request_and_return_io(pipeline, request)
+          response.body.should contain ip_address2
+        end
+      end
+    end
+  end
+end
---
+++ src/amber/pipes/client_ip.cr
@@ -0,0 +1,34 @@
+module Amber
+  module Pipe
+    # Handler to determine real client IP address when Amber is behind a
+    # reverse proxy and save it to context.client_ip.
+    #
+    # The proxy should pass client IP in a header such as "X-Forwarded-For".
+    # In case of multiple values in a header, the last one is used.
+    #
+    # Crystal does not currently make remote IP available, so it is impossible
+    # to accept the header value only when coming from trusted proxy IPs.
+    # For that and other benefits, it is suggested to always run Amber apps
+    # behind proxies such as HAProxy or Nginx.
+    class ClientIp < Base
+      def initialize(header : String = "X-Forwarded-For")
+        @headers = [header]
+      end
+      def initialize( @headers : Array(String))
+      end
+
+      def call(context : HTTP::Server::Context)
+        @headers.each do |header|
+          if addresses = context.request.headers.get?(header)
+            context.client_ip = Socket::IPAddress.new(addresses[-1], 0)
+          end
+        end
+        call_next(context)
+      end
+    end
+  end
+end
+
+class HTTP::Server::Context
+  property client_ip : Socket::IPAddress?
+end
