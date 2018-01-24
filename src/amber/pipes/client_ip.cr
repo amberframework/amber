82eecb23d23094457f9ee8c14d2db7e102d166f1
Davor Ocelic <docelic@spinlocksolutions.com>
2018-01-24 19:38:04 +0100

    Add client IP pipe (#564)
    * Remove unconditional search for real client IP (#534)
    
    * Pipe::ClientIp - saves client IP to Amber context
    
    - Add Amber::Pipe::ClientIp
    - It looks into configured header names (default: "X-Forwarded-For"),
      finds the first header that exists, and takes its last value
    - Saves the value into context.client_ip as Socket::IPAddress(addr, 0)
    
    - Covered by specs
    
    - Along the way, sort in alphabetic order methods on the controller
      that are delegated to context for easier lookup and modification
    
    * Add ClientIp pipe template to config/routes.cr
    
    - Add Amber::Pipe::ClientIp to template config/routes.cr
    - By default, add it in disabled state
    - Even though ClientIp has default header ("X-Forward-For"), do mention
      name explicitly in the template, and also show an array to be as
      easy and intuitive as possible for users to understand what the header
      name is and that multiple headers are accepted
    
    * Use first instead of last IP address (#564)
    
    * Make non-matched header name more obvious (#564)
    
    * Improvements as per discussion #564
    
    - Fix/improve examples
    - Extract HTTP::Server::Context extensions to extensions/http_extensions.cr

---
+++ src/amber/pipes/client_ip.cr
@@ -0,0 +1,30 @@
+module Amber
+  module Pipe
+    # Handler to determine real client IP address when Amber is behind
+    # trusted reverse proxy or proxies, and save it to context.client_ip.
+    #
+    # Reverse proxy should pass client IP in a header such as "X-Forwarded-For".
+    # In case of multiple values in a header, the leftmost one is used.
+    #
+    # For this functionality to not cause security concerns, all trusted,
+    # public-facing reverse proxies should delete the header if request is
+    # coming from an untrusted client before appending their own, valid value.
+    class ClientIp < Base
+
+      def initialize(header : String = "X-Forwarded-For")
+        @headers = [header]
+      end
+      def initialize( @headers : Array(String))
+      end
+
+      def call(context : HTTP::Server::Context)
+        @headers.each do |header|
+          if addresses = context.request.headers.get?(header)
+            context.client_ip = Socket::IPAddress.new(addresses[0], 0)
+          end
+        end
+        call_next(context)
+      end
+    end
+  end
+end
