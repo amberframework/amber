aded506020741f358d3381dac01089f28d5421d1
Davor Ocelic <docelic@spinlocksolutions.com>
2018-02-25 01:08:30 +0100

    Update src/handlers/error.cr.ecr and src/amber/pipes/error.cr
    The files /src/amber/cli/templates/error/src/handlers/error.cr.ecr
    and src/amber/pipes/error.cr are pretty similar but have desynced
    a little bit.
    
    This commit fixes the textual descriptions and adds a missing
    begin to make the files as similar as possible.
    
    The next step will be to change location from /handlers/ in the
    user application to /pipes/.

--- src/amber/pipes/error.cr
+++ src/amber/pipes/error.cr
@@ -5,7 +5,7 @@ module Amber
     # Exceptions and returns a backtrace in text/html format.
     class Error < Base
       def call(context : HTTP::Server::Context)
-        raise Amber::Exceptions::RouteNotFound.new(context.request) if context.invalid_route?
+        raise Amber::Exceptions::RouteNotFound.new(context.request) unless context.valid_route?
         call_next(context)
       rescue ex : Amber::Exceptions::Forbidden
         context.response.status_code = 403
