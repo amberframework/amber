7065e3a7225936578db50779e4887ddf8031e60d
Elias J. Perez <eliasjpr@gmail.com>
2018-03-03 09:34:12 -0500

    [Params] Patches params #to_h
    Fixes params #to_h method to return a flat hash of params.

--- src/amber/router/params.cr
+++ src/amber/router/params.cr
@@ -61,13 +61,11 @@ module Amber::Router
     end
 
     def to_h
-      {
-        :multipart => multipart.to_h,
-        :json      => json.to_h,
-        :route     => route.to_h,
-        :form      => form.to_h,
-        :query     => query.to_h,
-      }
+      query.to_h
+        .merge(form.to_h)
+        .merge(route)
+        .merge(json)
+        .merge(multipart)
     end
 
     private def query
