a6e548b1a85bd0272239c81ccff22e246ea38629
Eloy <ej.perezgomez@gmail.com>
2018-03-08 13:38:33 +0100

    Move LoggerBuilder into its own file

--- src/amber/environment/settings.cr
+++ src/amber/environment/settings.cr
@@ -109,29 +109,4 @@ module Amber::Environment
       @_logging ||= Logging.new(@logging)
     end
   end
-
-  class LoggerBuilder
-    def self.logger(io, logging)
-      new(io, logging).logger
-    end
-
-    def initialize(io, logging)
-      Colorize.enabled = logging.colorize
-      @logger = Environment::Logger.new(io)
-      @logger.level = logging.severity
-      @logger.progname = "Server"
-      @logger.formatter = Logger::Formatter.new do |severity, datetime, progname, message, io|
-        io << datetime.to_s("%I:%M:%S")
-        io << " "
-        io << progname
-        io << " (#{severity})" if severity > Logger::DEBUG
-        io << " "
-        io << message
-      end
-    end
-
-    def logger
-      @logger
-    end
-  end
 end
