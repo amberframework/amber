506400c32d893f34d218eb8cfac04966eafab8d0
Eloy <ej.perezgomez@gmail.com>
2018-03-08 13:49:57 +0100

    Use getter macro to generate logger method

--- src/amber/environment/logger_builder.cr
+++ src/amber/environment/logger_builder.cr
@@ -1,12 +1,14 @@
 module Amber::Environment
   class LoggerBuilder
+    getter logger : Logger
+
     def self.logger(io, logging)
       new(io, logging).logger
     end
 
     def initialize(io, logging)
       Colorize.enabled = logging.colorize
-      @logger = Environment::Logger.new(io)
+      @logger = Logger.new(io)
       @logger.level = logging.severity
       @logger.progname = "Server"
       @logger.formatter = Logger::Formatter.new do |severity, datetime, progname, message, io|
@@ -17,10 +19,6 @@ module Amber::Environment
         io << " "
         io << message
       end
-    end
-
-    def logger
-      @logger
     end
   end
 end
