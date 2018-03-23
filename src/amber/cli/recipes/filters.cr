1b6828ae5dfd78964f4780dee2ee4439c346128e
Damian Hamill <damianham@gmail.com>
2018-03-23 16:30:16 +0700

    add pluralize and underscore liquid filters

---
+++ src/amber/cli/recipes/filters.cr
@@ -0,0 +1,54 @@
+# add a pluralize filter to liquid
+# this should be a temporary measure until pluralize is available as a filter
+# in the liquid shard
+
+require "inflector"
+
+module Liquid::Filters
+
+  FilterRegister.register "pluralize", Pluralize
+  FilterRegister.register "underscore", Underscore
+
+  # pluralize
+  #
+  # Uses the Inflector to pluralize a string.
+  #
+  # Input
+  # {{ "post" | pluralize }}
+  #
+  # Output
+  # posts
+  class Pluralize
+    extend Filter
+
+    def self.filter(data : Any, args : Array(Any)? = nil) : Any
+      if (raw = data.raw) && raw.is_a? String
+        Any.new Inflector.pluralize(raw)
+      else
+        data
+      end
+    end
+  end
+
+  # underscore
+  #
+  # Uses the Inflector to underscore a string.
+  #
+  # Input
+  # {{ "ActiveModel" | underscore }}
+  #
+  # Output
+  # active_model
+  class Underscore
+    extend Filter
+
+    def self.filter(data : Any, args : Array(Any)? = nil) : Any
+      if (raw = data.raw) && raw.is_a? String
+        Any.new Inflector.underscore(raw)
+      else
+        data
+      end
+    end
+  end
+
+end
