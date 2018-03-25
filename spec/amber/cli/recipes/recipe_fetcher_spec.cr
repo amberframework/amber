b33a4abd0b60e463d4bab8ed9a2dc3601e1992db
Damian Hamill <damianham@gmail.com>
2018-03-25 18:21:09 +0700

    fix styling for ameba

--- spec/amber/cli/recipes/recipe_fetcher_spec.cr
+++ spec/amber/cli/recipes/recipe_fetcher_spec.cr
@@ -18,17 +18,17 @@ module Amber::Recipes
     describe "RecipeFetcher" do
 
       it "should use a local app folder" do
-        template = RecipeFetcher.new("app", "mydefault").fetch 
+        template = RecipeFetcher.new("app", "mydefault").fetch
         template.should_not be nil
         template.should match(/.+mydefault\/app$/)
       end
-      
+
       it "should use a local controller folder" do
-        template = RecipeFetcher.new("controller", "mydefault").fetch 
+        template = RecipeFetcher.new("controller", "mydefault").fetch
         template.should_not be nil
         template.should match(/.+mydefault\/controller$/)
       end
-      
+
     end
   end
 end
