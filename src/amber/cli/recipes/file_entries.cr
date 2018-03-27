01db9dde2425283ec50e7557a989a4b7ab0e5969
Damian Hamill <damianham@gmail.com>
2018-03-27 15:37:02 +0700

    use more descriptive variable names and remove redundant comments

--- src/amber/cli/recipes/file_entries.cr
+++ src/amber/cli/recipes/file_entries.cr
@@ -2,62 +2,62 @@ require "teeplate"
 
 module Amber::Recipes
   module FileEntries
-    def each_file(abs, rel, &block : String, String ->)
-      Dir.open(abs) do |d|
-        d.each_child do |entry|
-          each_file abs, rel, entry, &block
+    def each_file(absolute_path, filename, &block : String, String ->)
+      Dir.open(absolute_path) do |directory|
+        directory.each_child do |entry|
+          each_file absolute_path, filename, entry, &block
         end
       end
     end
 
-    def each_file(abs, rel, entry, &block : String, String ->)
-      rel = rel ? File.join(rel, entry) : entry
-      abs = File.join(abs, entry)
-      if Dir.exists?(abs)
-        each_file abs, rel, &block
+    def each_file(absolute_path, filename, entry, &block : String, String ->)
+      filename = filename ? File.join(filename, entry) : entry
+      absolute_path = File.join(absolute_path, entry)
+      if Dir.exists?(absolute_path)
+        each_file absolute_path, filename, &block
       else
-        block.call abs, rel
+        block.call absolute_path, filename
       end
     end
 
-    def pack_liquid(files, abs, rel)
-      tpl = Liquid::Template.parse File.new(abs)
-      io = tpl.render @ctx.as(Liquid::Context)
-      files << ::Teeplate::StringData.new(rel, io.to_s, File.stat(abs).perm)
+    def pack_liquid(files, absolute_path, filename)
+      template = Liquid::Template.parse File.new(absolute_path)
+      io = template.render @ctx.as(Liquid::Context)
+      files << ::Teeplate::StringData.new(filename, io.to_s, File.stat(absolute_path).perm)
     rescue ex
-      p "failed to process #{abs} - #{ex.message}"
+      CLI.logger.error "failed to process #{absolute_path} - #{ex.message}"
     end
 
-    def pack_blob(files, abs, rel)
+    def pack_blob(files, absolute_path, filename)
       io = IO::Memory.new
-      File.open(abs) { |f| IO.copy(f, io) }
+      File.open(absolute_path) { |f| IO.copy(f, io) }
 
-      files << ::Teeplate::Base64Data.new(rel, io.size.to_u64, Base64.encode(io), File.stat(abs).perm)
+      files << ::Teeplate::Base64Data.new(filename, io.size.to_u64, Base64.encode(io), File.stat(absolute_path).perm)
     end
 
     def collect_files(files)
+
       if ! @template.nil?
 
         @ctx = Liquid::Context.new
         set_context @ctx
 
-        each_file(@template.as(String), nil) do |abs, rel|
+        each_file(@template.as(String), nil) do |absolute_path, filename|
           # process the filename with liquid
-          tpl = Liquid::Template.parse rel
-          rel = tpl.render @ctx.as(Liquid::Context)
+          template = Liquid::Template.parse filename
+          filename = template.render @ctx.as(Liquid::Context)
 
-          if /^(.+)\.lqd$|^(.+)\.liquid$/ =~ rel
+          if /^(.+)\.lqd$|^(.+)\.liquid$/ =~ filename
             # process the file with liquid
-            pack_liquid files, abs, $1
+            pack_liquid files, absolute_path, $1
           else
             # pack the file without processing
-            pack_blob files, abs, rel
+            pack_blob files, absolute_path, filename
           end
         end
       end
     end
 
-    # Returns collected file entries.
     def file_entries : Array(Teeplate::AsDataEntry)
       @file_entries ||= begin
         files = [] of Teeplate::AsDataEntry
