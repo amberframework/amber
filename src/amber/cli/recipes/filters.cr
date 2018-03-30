a065bcbba6f81ee63f179b7fbb5f56fe6c9799d0
Damian Hamill <damianham@gmail.com>
2018-03-30 19:32:28 +0700

    add camelize/camelcase filter

--- src/amber/cli/recipes/filters.cr
+++ src/amber/cli/recipes/filters.cr
@@ -4,6 +4,37 @@ module Liquid::Filters
 
   FilterRegister.register "pluralize", Pluralize
   FilterRegister.register "underscore", Underscore
+  FilterRegister.register "camelcase", CamelCase
+  FilterRegister.register "camelize", CamelCase
+
+  # Uses the Inflector to camelize/camelcase a string.
+  #
+  # If the +uppercase_first_letter+ parameter is set to false, then produces
+  # lowerCamelCase.
+  #
+  # Also converts "/" to "::" which is useful for converting
+  # paths to namespaces.
+  #
+  #   camelize("active_model")                # => "ActiveModel"
+  #   camelize("active_model", false)         # => "activeModel"
+  #   camelize("active_model/errors")         # => "ActiveModel::Errors"
+  #   camelize("active_model/errors", false)  # => "activeModel::Errors"
+  #
+  # As a rule of thumb you can think of +camelize+ as the inverse of
+  # #underscore, though there are cases where that does not hold:
+  #
+  #   camelize(underscore("SSLError"))        # => "SslError"
+  class CamelCase
+    extend Filter
+
+    def self.filter(data : Any, args : Array(Any)? = nil) : Any
+      if (raw = data.raw) && raw.is_a? String
+        Any.new Inflector.camelize(raw)
+      else
+        data
+      end
+    end
+  end
 
   # pluralize
   #
