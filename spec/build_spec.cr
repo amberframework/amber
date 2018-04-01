8301c887e89dbc461064ed96d0386a0fe1ca1da9
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2018-03-31 19:59:17 -0500

    Remove trailing spaces

--- spec/build_spec.cr
+++ spec/build_spec.cr
@@ -51,18 +51,18 @@ module Amber::CLI
       puts "RUNNING: shard build #{TESTING_APP} - started..."
       build_result = `shards build #{TEST_APP_NAME}`
       puts "#{TESTING_APP} build completed..."
-      
+
       finish_setup
-    
+
       it "generates a binary" do
         puts build_result unless File.exists?("bin/#{TEST_APP_NAME}")
         File.exists?("bin/#{TEST_APP_NAME}").should be_true
       end
 
       context "crystal spec" do
-        
+
         wait_setup
-        
+
         puts "RUNNING: crystal spec #{TESTING_APP} - started..."
         spec_result = `crystal spec`
 
