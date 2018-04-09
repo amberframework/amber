ba70289c99aaae10fd141e65a27d91bed67b533c
Damian Hamill <damianham@gmail.com>
2018-04-09 20:36:25 +0700

    [CLI] add support for Recipes (squashed commits) (#728)
    * [CLI] add support for recipes
    
    * add a recipe option to amber new|generate to create applications and
      scaffolding from recipes
    
    Issue: https://github.com/amberframework/amber/issues/629
    
    - adds TechMagister/liquid template language to shards
    - adds a recipes folder to amber/cli
    - adds recipe option to cli and saves the recipe name|path in .amber.yml
    - adds support for recipes to amber/cli/commands/[new|generate].cr
    - adds to_json method to amber/cli/templates/field.cr
    
    This adds the ability to generate applications and scaffolding from a recipe
    given as a recipe name or a file system path.  Recipes given as a recipe name
    are downloaded from amberframework/recipes repository.
    
    * [CLI] enhancements to add support for recipes
    * improvements to enhance the recipes feature
    
    - recipe cache in $HOME/.amber/recipe_cache
    - app generator views filter only blocks .ecr or .slang
    - fix a typo, class_name is a method
    - add uppercase filter
    
    This places the recipe cache in the user's home folder so there will be a
    single instance of the cache rather than multiple instances. The app generator
    is changed so that it only filters out views for the template language that
    is not being generated to allow any other type of file in views e.g.
    javascript. Some generators where using @class_name instead of class_name,
    this typo is fixed.  A filter has been added to uppercase a string using
    upcase or uppercase.

--- src/amber/cli/commands.cr
+++ src/amber/cli/commands.cr
@@ -1,5 +1,6 @@
 require "../version"
 require "cli"
+require "./recipes/recipe"
 require "./templates/template"
 require "./config"
 require "./command"
@@ -47,6 +48,7 @@ module Amber::CLI
       string ["-t", "--template"], desc: "# Preconfigure for selected template engine. Options: slang | ecr", default: "slang"
       string ["-d", "--database"], desc: "# Preconfigure for selected database. Options: pg | mysql | sqlite", default: "pg"
       string ["-m", "--model"], desc: "# Preconfigure for selected model. Options: granite | crecto", default: "granite"
+      string ["-r", "--recipe"], desc: "# Use a named recipe.  See documentation at https://amberframework.org.", default: nil
     end
   end
 end
