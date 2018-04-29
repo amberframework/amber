d1f41582950b8bb62c43e7f6713d5c45d8b7f677
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2018-04-29 18:57:07 -0500

    Remove database check and enable logs to debug CI

--- spec/build_spec.cr
+++ spec/build_spec.cr
@@ -27,10 +27,7 @@ module Amber::CLI
 
       prepare_yaml(Dir.current)
       Amber::CLI.env = "test"
-      Amber::CLI.settings.logger = Amber::Environment::Logger.new(nil)
-
-      puts "WAITING: database connection..."
-      `while true; do pg_isready -q; if [ $? -eq 0 ]; then break; fi; sleep 0.1; done`
+      #Amber::CLI.settings.logger = Amber::Environment::Logger.new(nil)
 
       puts "====== START Creating database for #{TEST_APP_NAME} ======"
       MainCommand.run ["db", "drop"]
