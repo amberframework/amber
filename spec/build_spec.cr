dffcb53b3fcb25c002def0077329f138dfffeb1e
Faustino Aguilar <faustinoaq@gmail.com>
2018-04-30 16:26:40 -0500

    Trigger build

--- spec/build_spec.cr
+++ spec/build_spec.cr
@@ -80,3 +80,4 @@ end
 # Trigger build at Mon Apr 30 16:17:30 EST 2018
 # Trigger build at Mon Apr 30 16:20:33 EST 2018
 # Trigger build at Mon Apr 30 16:23:37 EST 2018
+# Trigger build at Mon Apr 30 16:26:40 EST 2018
