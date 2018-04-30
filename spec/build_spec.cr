f7dbcf78359941288751c81aa86c1e2958b66594
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2018-04-29 20:02:10 -0500

    Return macro and execute create migrate separetely

--- spec/build_spec.cr
+++ spec/build_spec.cr
@@ -1,3 +1,4 @@
+{% if flag?(:run_build_tests) %}
 require "./spec_helper"
 require "./support/helpers/cli_helper"
 
@@ -37,7 +38,8 @@ module Amber::CLI
 
       puts "====== START Creating database for #{TEST_APP_NAME} ======"
       MainCommand.run ["db", "drop"]
-      MainCommand.run ["db", "create", "migrate"]
+      MainCommand.run ["db", "create"]
+      MainCommand.run ["db", "migrate"]
       puts "====== DONE Database created #{TEST_APP_NAME} ======"
 
       it "generates a binary" do
@@ -69,3 +71,4 @@ module Amber::CLI
     cleanup
   end
 end
+{% end %}
