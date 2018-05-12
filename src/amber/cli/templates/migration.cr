e10dc0c9dcfa46a09829510457edbb501fee3a10
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2018-05-12 15:42:22 -0500

    Remove duplicated initialization

--- src/amber/cli/templates/migration.cr
+++ src/amber/cli/templates/migration.cr
@@ -15,7 +15,6 @@ module Amber::CLI
 
     def initialize(@name, fields)
       @fields = fields.map { |field| Field.new(field) }
-      @timestamp = Time.now.to_s("%Y%m%d%H%M%S%L")
       @fields = fields.map { |field| Field.new(field, database: @database) }
       @fields += %w(created_at:time updated_at:time).map do |f|
         Field.new(f, hidden: true, database: @database)
