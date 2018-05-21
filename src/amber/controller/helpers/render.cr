8802782336cc696af563bee37116fd222059f836
YORÇI <ileti@yorci.com>
2018-05-21 12:12:47 +0300

    Kemal's content_for and yield_content implementation
    Kemal's content_for and yield_content implementation

--- src/amber/controller/helpers/render.cr
+++ src/amber/controller/helpers/render.cr
@@ -1,6 +1,26 @@
 module Amber::Controller::Helpers
   module Render
     LAYOUT = "application.slang"
+    CONTENT_FOR_BLOCKS = Hash(String, Tuple(String, Proc(String))).new
+
+    macro content_for(key, file = __FILE__)
+      %proc = ->() {
+        __kilt_io__ = IO::Memory.new
+        {{ yield }}
+        __kilt_io__.to_s
+      }
+
+      CONTENT_FOR_BLOCKS[{{key}}] = Tuple.new {{file}}, %proc
+      nil
+    end
+    
+    macro yield_content(key)
+      if CONTENT_FOR_BLOCKS.has_key?({{key}})
+        __caller_filename__ = CONTENT_FOR_BLOCKS[{{key}}][0]
+        %proc = CONTENT_FOR_BLOCKS[{{key}}][1]
+        %proc.call if __content_filename__ == __caller_filename__
+      end
+    end
 
     macro render(template = nil, layout = true, partial = nil, path = "src/views", folder = __FILE__)
       {% if !(template || partial) %}
@@ -15,8 +35,10 @@ module Amber::Controller::Helpers
       {% else %}
         {{ short_path = folder.gsub(/^.+?(?:controllers|views)\//, "") }}
         {% if folder.id.ends_with?(".ecr") %}
+          __content_filename__ = "#{{{path}}}/#{{{short_path.gsub(/\/[^\.\/]+\.ecr/, "")}}}/#{{{filename}}}" # -- 
           %content = render_template("#{{{path}}}/#{{{short_path.gsub(/\/[^\.\/]+\.ecr/, "")}}}/#{{{filename}}}")
         {% else %}
+          __content_filename__ = "#{{{path}}}/#{{{short_path.gsub(/\_controller\.cr|\.cr/, "")}}}/#{{{filename}}}" # -- 
           %content = render_template("#{{{path}}}/#{{{short_path.gsub(/\_controller\.cr|\.cr/, "")}}}/#{{{filename}}}")
         {% end %}
       {% end %}
