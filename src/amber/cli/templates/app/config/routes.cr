923c3f376b59434d596e2632de622acdd8d5cba9
Dru Jensen <drujensen@gmail.com>
2018-06-13 10:10:10 -0700

    api generator (#828)
    * initial work on api generator
    
    * update controllers
    
    * wip on specs
    
    * use pluralize and provide errors
    
    * add errors to create
    
    * add api pipeline
    
    * fix respond_with
    
    * fix routes
    
    * working version
    
    * improve specs later

--- src/amber/cli/templates/app/config/routes.cr
+++ src/amber/cli/templates/app/config/routes.cr
@@ -14,16 +14,26 @@ Amber::Server.configure do
     plug Amber::Pipe::Reload.new if Amber.env.development?
   end
 
+  pipeline :api do
+    plug Amber::Pipe::PoweredByAmber.new
+    plug Amber::Pipe::Error.new
+    plug Amber::Pipe::Logger.new
+    plug Amber::Pipe::Session.new
+    plug Amber::Pipe::CORS.new
+  end
+
   # All static content will run these transformations
   pipeline :static do
     plug Amber::Pipe::PoweredByAmber.new
-    # plug Amber::Pipe::ClientIp.new(["X-Forwarded-For"])
     plug Amber::Pipe::Error.new
     plug Amber::Pipe::Static.new("./public")
   end
 
   routes :web do
     get "/", HomeController, :index
+  end
+
+  routes :api do
   end
 
   routes :static do
