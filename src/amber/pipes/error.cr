7f49bd7ad3dca09e06e39c72ef4a53322639e316
Faustino Aguilar <faustinoaq@gmail.com>
2018-06-16 14:15:24 -0500

    Add missing exception

--- src/amber/pipes/error.cr
+++ src/amber/pipes/error.cr
@@ -7,6 +7,10 @@ module Amber
       def call(context : HTTP::Server::Context)
         raise Amber::Exceptions::RouteNotFound.new(context.request) unless context.valid_route?
         call_next(context)
+      rescue ex : Amber::Exceptions::ValidationFailed | Amber::Exceptions::InvalidParam
+        context.response.status_code = 400
+        error = Amber::Controller::Error.new(context, ex)
+        context.response.print(error.bad_request)
       rescue ex : Amber::Exceptions::Forbidden
         context.response.status_code = 403
         error = Amber::Controller::Error.new(context, ex)
