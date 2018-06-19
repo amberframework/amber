65f81aa13b63fc4d921f51302f602db27f478b84
Faustino Aguilar <faustinoaq@gmail.com>
2018-06-19 12:18:22 -0500

    Remove trailing whitespaces

--- src/amber/cli/helpers/file_watcher.cr
+++ src/amber/cli/helpers/file_watcher.cr
@@ -1,11 +1,11 @@
 module Amber
     class FileWatcher
       @file_timestamps = {} of String => String
-  
+
       private def get_timestamp(file : String)
         File.info(file).modification_time.to_s("%Y%m%d%H%M%S")
       end
-  
+
       def scan_files(files)
         Dir.glob(files) do |file|
           timestamp = get_timestamp(file)
@@ -17,4 +17,3 @@ module Amber
       end
     end
   end
-  
