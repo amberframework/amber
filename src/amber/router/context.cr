35fe1d8cf3c217caa34cd48ed5801a0a6b37f183
Elias Perez <eliasjpr@users.noreply.github.com>
2018-09-05 20:49:02 -0400

    fixup!

--- src/amber/router/context.cr
+++ src/amber/router/context.cr
@@ -79,7 +79,7 @@ class HTTP::Server::Context
 
   protected def finalize_response
     response.headers["Connection"] = "Keep-Alive"
-    response.headers.add("Keep-Alive", "timeout=5, max=10000")res
+    response.headers.add("Keep-Alive", "timeout=5, max=10000")
     unless response.headers["Content-Type"]?
       response.headers["Content-Type"] = Amber::Support::Mime.mime_type(format)
     end
