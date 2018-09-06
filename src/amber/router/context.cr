a34bb54c30e78ebc56b3c1684ab551f2f6c07ac4
Elias Perez <eliasjpr@users.noreply.github.com>
2018-09-05 20:47:59 -0400

    [Router] Ensures Content-Type header for requests
    Issue: https://github.com/amberframework/amber/issues/947
    
    Ensures a content-type header is added for all requests. Content-Type defaults to the requested format mime type.

--- src/amber/router/context.cr
+++ src/amber/router/context.cr
@@ -79,7 +79,10 @@ class HTTP::Server::Context
 
   protected def finalize_response
     response.headers["Connection"] = "Keep-Alive"
-    response.headers.add("Keep-Alive", "timeout=5, max=10000")
+    response.headers.add("Keep-Alive", "timeout=5, max=10000")res
+    unless response.headers["Content-Type"]?
+      response.headers["Content-Type"] = Amber::Support::Mime.mime_type(format)
+    end
     response.print(@content) unless request.method == "HEAD"
   end
 end
