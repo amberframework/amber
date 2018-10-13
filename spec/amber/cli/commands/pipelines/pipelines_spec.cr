07ed7940cc99a4f126cbae9767726ec5cf836873
Sam Holst <sawohol@gmail.com>
2018-10-13 11:38:23 -0700

    added in pipeline to the spec

--- spec/amber/cli/commands/pipelines/pipelines_spec.cr
+++ spec/amber/cli/commands/pipelines/pipelines_spec.cr
@@ -28,6 +28,7 @@ module Amber::CLI
           Amber::Pipe::Session
           Amber::Pipe::Flash
           Amber::Pipe::CSRF
+          Amber::Pipe::Reload
         )
 
         static_default_plugs = %w(
