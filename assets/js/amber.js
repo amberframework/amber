c40e2f1f180f78d71d1742d82ecf700737372a09
Dru Jensen <drujensen@gmail.com>
2018-11-28 04:08:04 -0800

    remove forEach

--- assets/js/amber.js
+++ assets/js/amber.js
@@ -232,23 +232,25 @@ module.exports = {
  * Allows delete links to post for security and ease of use similar to Rails jquery_ujs
  */
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll("a[data-method='delete']").forEach(function (element) {
-        element.addEventListener("click", function (e) {
-            e.preventDefault();
-            var message = element.getAttribute("data-confirm") || "Are you sure?";
-            if (confirm(message)) {
-                var form = document.createElement("form");
-                var input = document.createElement("input");
-                form.setAttribute("action", element.getAttribute("href"));
-                form.setAttribute("method", "POST");
-                input.setAttribute("type", "hidden");
-                input.setAttribute("name", "_method");
-                input.setAttribute("value", "DELETE");
-                form.appendChild(input);
-                document.body.appendChild(form);
-                form.submit();
-            }
-            return false;
-        })
+  var elements = document.querySelectorAll("a[data-method='delete']");
+  var i;
+  for (i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", function (e) {
+      e.preventDefault();
+      var message = elements[i].getAttribute("data-confirm") || "Are you sure?";
+      if (confirm(message)) {
+        var form = document.createElement("form");
+        var input = document.createElement("input");
+        form.setAttribute("action", elements[i].getAttribute("href"));
+        form.setAttribute("method", "POST");
+        input.setAttribute("type", "hidden");
+        input.setAttribute("name", "_method");
+        input.setAttribute("value", "DELETE");
+        form.appendChild(input);
+        document.body.appendChild(form);
+        form.submit();
+      }
+      return false;
     })
+  }
 });
