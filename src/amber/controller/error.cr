2cf39e7921ca48b7bd2352a4bc138506ed9061f0
Dwight Watson <dwightwatson@users.noreply.github.com>
2018-12-14 03:33:10 +1300

    Use exception_page shard (#991)
    * Use exception_page shard
    
    * Restore existing error controller
    
    * Restore Error page
    
    * Attempt to restore suite
    
    * Only return exception page in development
    
    * Move exception_page dependency to app template

--- src/amber/controller/error.cr
+++ src/amber/controller/error.cr
@@ -1,4 +1,5 @@
 require "./base"
+require "../exceptions/page"
 
 module Amber::Controller
   class Error < Base
@@ -7,49 +8,46 @@ module Amber::Controller
       @context.response.content_type = content_type
     end
 
-    def not_found
-      response_format(@ex.message)
-    end
-
-    def internal_server_error
-      response_format("ERROR: #{internal_server_error_message}")
+    def bad_request
+      response_format
     end
 
     def forbidden
-      response_format(@ex.message)
+      response_format
+    end
+
+    def not_found
+      response_format
+    end
+
+    def internal_server_error
+      response_format
     end
 
     private def content_type
       if context.request.headers["Accept"]?
         request.headers["Accept"].split(",").first
       else
-        "text/html"
+        "text/plain"
       end
     end
 
-    private def internal_server_error_message
-      # IMPORTANT: #inspect_with_backtrace will fail in some situations which breaks the tests.
-      # Even if you call @ex.callstack you'll notice that backtrace is nil.
-      # #backtrace? is supposed to be safe but it exceptions anyway.
-      # Please don't remove this without verifying that crystal core has been fixed first.
-      @ex.inspect_with_backtrace
-    rescue ex : IndexError
-      @ex.message
-    rescue ex
-      <<-ERROR
-      Original Error: #{@ex.message}
-      Error during 'inspect_with_backtrace': #{ex.message}
-      ERROR
-    end
-
-    private def response_format(message)
+    private def response_format
       case content_type
       when "application/json"
-        {"error": message}.to_json
+        {"error": @ex.message}.to_json
       when "text/html"
-        "<html><body><pre>#{message}</pre></body></html>"
+        html_response
       else
-        message
+        @ex.message
+      end
+    end
+
+    private def html_response
+      if Amber.env.development?
+        Amber::Exceptions::Page.for_runtime_exception(context, @ex).to_s
+      else
+        "<html><body><pre>#{@ex.message}</pre></body></html>"
       end
     end
   end
