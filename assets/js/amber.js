amberframework/amber

commit 238d5a09937fff898d82e9ff116a787fd687292d

Prefer JavaScript UMD modules spec (#1028)
It appears that Webpack is already handling the UMD import/export specification correctly when compiling the assets within Amber. In the amber.js websocket file, both UMD and common JS are present, and non-identical as both `class Channel` and `class Socket` are exported in the UMD format but only Socket is exported as CommonJS. A side-effect of this is that the `module.exports` object is transpiled into the bundle as Webpack has already defaulted to using UMD.

This commit simply removes the CommonJS export definition.

assets/js/amber.js
@@ -223,11 +223,6 @@ export class Socket {
   }
 }
 
-module.exports = {
-  Socket: Socket
-}
-
-
 /**
  * Allows delete links to post for security and ease of use similar to Rails jquery_ujs
  */

commit 6e4769f0232e4620fef69bbab5246c556fdc467f

Re-add default export to Amber.js
Regression in https://github.com/amberframework/amber/pull/1028/files when aligning the CommonJS and UMD spec of the Amber library.

The current import statement in main.js in a fresh project return `Amber` as undefined. It will work with `import * as Amber from 'amber;'` although both would be preferable.

assets/js/amber.js
@@ -223,6 +223,11 @@ export class Socket {
   }
 }
 
+export default {
+  Channel,
+  Socket
+};
+
 /**
  * Allows delete links to post for security and ease of use similar to Rails jquery_ujs
  */