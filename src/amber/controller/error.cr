60519953bf3747c735b4b4bf950357fb5d565158
Weston Ganger <weston@westonganger.com>
2019-02-04 22:51:27 -0800

    Make Generated ErrorController inherit from ApplicationController (#1052)

--- src/amber/controller/error.cr
+++ src/amber/controller/error.cr
@@ -8,47 +8,51 @@ module Amber::Controller
       @context.response.content_type = content_type
     end
 
-    def bad_request
-      response_format
-    end
+    module Helpers
+      def bad_request
+        response_format
+      end
 
-    def forbidden
-      response_format
-    end
+      def forbidden
+        response_format
+      end
 
-    def not_found
-      response_format
-    end
+      def not_found
+        response_format
+      end
 
-    def internal_server_error
-      response_format
-    end
+      def internal_server_error
+        response_format
+      end
 
-    private def content_type
-      if context.request.headers["Accept"]?
-        request.headers["Accept"].split(",").first
-      else
-        "text/plain"
+      private def content_type
+        if context.request.headers["Accept"]?
+          request.headers["Accept"].split(",").first
+        else
+          "text/plain"
+        end
+      end
+
+      private def response_format
+        case content_type
+        when "application/json"
+          {"error": @ex.message}.to_json
+        when "text/html"
+          html_response
+        else
+          @ex.message
+        end
+      end
+
+      private def html_response
+        if Amber.env.development?
+          Amber::Exceptions::Page.for_runtime_exception(context, @ex).to_s
+        else
+          "<html><body><pre>#{@ex.message}</pre></body></html>"
+        end
       end
     end
 
-    private def response_format
-      case content_type
-      when "application/json"
-        {"error": @ex.message}.to_json
-      when "text/html"
-        html_response
-      else
-        @ex.message
-      end
-    end
-
-    private def html_response
-      if Amber.env.development?
-        Amber::Exceptions::Page.for_runtime_exception(context, @ex).to_s
-      else
-        "<html><body><pre>#{@ex.message}</pre></body></html>"
-      end
-    end
+    include Amber::Controller::Error::Helpers
   end
 end
