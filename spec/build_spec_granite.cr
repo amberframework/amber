3c3a6d309148081752a44f058c63e5cee4a7b431
Dru Jensen <drujensen@gmail.com>
2019-09-28 10:25:14 -0700

    ecr specs

--- spec/build_spec_granite.cr
+++ spec/build_spec_granite.cr
@@ -32,4 +32,36 @@ module Amber::CLI
       cleanup
     end
   end
+
+  describe "building a generated app using granite model and ecr template" do
+    it "check formatting" do
+      generate_app("-t", "ecr")
+      system("crystal tool format --check").should be_true
+      cleanup
+    end
+
+    it "shards update - dependencies" do
+      generate_app("-t", "ecr")
+      system("shards update").should be_true
+      cleanup
+    end
+
+    it "shards check - dependencies" do
+      generate_app("-t", "ecr")
+      system("shards check").should be_true
+      cleanup
+    end
+
+    it "shards build - generates a binary" do
+      generate_app("-t", "ecr")
+      system("shards build #{TEST_APP_NAME}").should be_true
+      cleanup
+    end
+
+    it "executes specs" do
+      generate_app("-t", "ecr")
+      system("crystal spec").should be_true
+      cleanup
+    end
+  end
 end
