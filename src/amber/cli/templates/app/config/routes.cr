4bf21a7e612d816612908f07c6a1f270e13413b0
Dru Jensen <drujensen@gmail.com>
2019-10-11 19:42:39 -0700

    fix powered by spec

--- src/amber/cli/templates/app/config/routes.cr
+++ src/amber/cli/templates/app/config/routes.cr
@@ -13,7 +13,7 @@ Amber::Server.configure do
   end
 
   pipeline :api do
-    plug Amber::Pipe::PoweredByAmber.new
+    # plug Amber::Pipe::PoweredByAmber.new
     plug Amber::Pipe::Error.new
     plug Amber::Pipe::Logger.new
     plug Amber::Pipe::Session.new
@@ -22,7 +22,7 @@ Amber::Server.configure do
 
   # All static content will run these transformations
   pipeline :static do
-    plug Amber::Pipe::PoweredByAmber.new
+    # plug Amber::Pipe::PoweredByAmber.new
     plug Amber::Pipe::Error.new
     plug Amber::Pipe::Static.new("./public")
   end
