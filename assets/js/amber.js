ca4f3088163fcef0fc4e47a26d9f1141485e0046
github username <github email address>
2019-10-24 17:14:50 -0700

    removed use of 'var', converted to arrow functions and moved 'i' declaration into 'for' arguments

--- assets/js/amber.js
+++ assets/js/amber.js
@@ -231,16 +231,15 @@ export default {
 /**
  * Allows delete links to post for security and ease of use similar to Rails jquery_ujs
  */
-document.addEventListener("DOMContentLoaded", function () {
-  var elements = document.querySelectorAll("a[data-method='delete']");
-  var i;
-  for (i = 0; i < elements.length; i++) {
-    elements[i].addEventListener("click", function (e) {
+document.addEventListener("DOMContentLoaded", () => {
+  let elements = document.querySelectorAll("a[data-method='delete']");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", (e) => {
       e.preventDefault();
-      var message = e.target.getAttribute("data-confirm") || "Are you sure?";
+      let message = e.target.getAttribute("data-confirm") || "Are you sure?";
       if (confirm(message)) {
-        var form = document.createElement("form");
-        var input = document.createElement("input");
+        let form = document.createElement("form");
+        let input = document.createElement("input");
         form.setAttribute("action", e.target.getAttribute("href"));
         form.setAttribute("method", "POST");
         input.setAttribute("type", "hidden");
@@ -256,9 +255,9 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 if (!Date.prototype.toGranite) {
-  (function() {
+  (() => {
 
-    function pad(number) {
+    let pad = (number) => {
       if (number < 10) {
         return '0' + number;
       }
@@ -273,6 +272,5 @@ if (!Date.prototype.toGranite) {
         ':' + pad(this.getUTCMinutes()) +
         ':' + pad(this.getUTCSeconds())  ;
     };
-
-  }());
+  };
 }
