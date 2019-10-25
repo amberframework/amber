01956c522199c7d62f7b8bd44d61db9d2249de8d
Sean Watters <sean@watters.io>
2019-10-25 14:07:57 -0700

    Updating 'Allows delete links to post' to ES6 syntax (#1161)
    * removed use of 'var', converted to arrow functions and moved 'i' declaration into 'for' arguments
    
    * missed a close parentheses
    
    * reversing changes to lines 258, 260 and 275

--- assets/js/amber.js
+++ assets/js/amber.js
@@ -231,16 +231,15 @@ export default {
 /**
  * Allows delete links to post for security and ease of use similar to Rails jquery_ujs
  */
-document.addEventListener("DOMContentLoaded", function () {
-  var elements = document.querySelectorAll("a[data-method='delete']");
-  var i;
-  for (i = 0; i < elements.length; i++) {
-    elements[i].addEventListener("click", function (e) {
+document.addEventListener("DOMContentLoaded", () => {
+  let elements = document.querySelectorAll("a[data-method='delete']");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener("click", (e) => {
       e.preventDefault();
-      var message = e.target.getAttribute("data-confirm") || "Are you sure?";
+      let message = e.target.getAttribute("data-confirm") || "Are you sure?";
       if (confirm(message)) {
-        var form = document.createElement("form");
-        var input = document.createElement("input");
+        let form = document.createElement("form");
+        let input = document.createElement("input");
         form.setAttribute("action", e.target.getAttribute("href"));
         form.setAttribute("method", "POST");
         input.setAttribute("type", "hidden");
