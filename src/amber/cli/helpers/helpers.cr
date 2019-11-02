1f83cea684445b28bdbb5ad0111dbd4c077aae1c
github username <sean@watters.io>
2019-11-01 18:46:01 -0700

    Changed 'add_routes' logic to check for symbol

--- src/amber/cli/helpers/helpers.cr
+++ src/amber/cli/helpers/helpers.cr
@@ -1,7 +1,7 @@
 module Amber::CLI::Helpers
   def add_routes(pipeline, route)
     routes = File.read("./config/routes.cr")
-    if pipeline.to_s == ":web"
+    if pipeline == :web
       replacement = <<-ROUTES
       routes :web do
       #{route}
