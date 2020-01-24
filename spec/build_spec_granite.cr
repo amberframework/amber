3fcc6cade87db5d9469a22843fc88111f4328184
Elias Perez <eliasjpr@users.noreply.github.com>
2020-01-24 16:48:34 -0500

    Update CI/deps to support Crystal 0.32.1 (#1173)
    * [No Ticket] Updates Dependencies
    
    New releases for DB, PG, MySQL and SQLite3 have been release that are
    breaking building new amber apps.
    
    This updates the shard.yml to use the latest versions for these projects
    
    Cleanup compose file and granite spec
    
    Cleanup once per context
    
    Use Ameba v0.11.0
    
    Remove commit and ~> from versions
    
    Remove ~> from shard yanml
    
    Bump micrate to 0.5.0
    
    Remove ~> from micrate
    
    Bump amber version
    
    Remove shards check
    
    * Update version in .crystal-version

--- spec/build_spec_granite.cr
+++ spec/build_spec_granite.cr
@@ -2,66 +2,44 @@ require "./build_spec_helper"
 
 module Amber::CLI
   describe "building a generated app using granite model and slang template" do
+    cleanup
+    generate_app("-t", "slang")
+
     it "check formatting" do
-      generate_app("-t", "slang")
       system("crystal tool format --check").should be_true
-      cleanup
     end
 
     it "shards update - dependencies" do
-      generate_app("-t", "slang")
       system("shards update").should be_true
-      cleanup
-    end
-
-    it "shards check - dependencies" do
-      generate_app("-t", "slang")
-      system("shards check").should be_true
-      cleanup
     end
 
     it "shards build - generates a binary" do
-      generate_app("-t", "slang")
       system("shards build #{TEST_APP_NAME}").should be_true
-      cleanup
     end
 
     it "executes specs" do
-      generate_app("-t", "slang")
       system("crystal spec").should be_true
-      cleanup
     end
   end
 
   describe "building a generated app using granite model and ecr template" do
+    cleanup
+    generate_app("-t", "ecr")
+
     it "check formatting" do
-      generate_app("-t", "ecr")
       system("crystal tool format --check").should be_true
-      cleanup
     end
 
     it "shards update - dependencies" do
-      generate_app("-t", "ecr")
       system("shards update").should be_true
-      cleanup
-    end
-
-    it "shards check - dependencies" do
-      generate_app("-t", "ecr")
-      system("shards check").should be_true
-      cleanup
     end
 
     it "shards build - generates a binary" do
-      generate_app("-t", "ecr")
       system("shards build #{TEST_APP_NAME}").should be_true
-      cleanup
     end
 
     it "executes specs" do
-      generate_app("-t", "ecr")
       system("crystal spec").should be_true
-      cleanup
     end
   end
 end
