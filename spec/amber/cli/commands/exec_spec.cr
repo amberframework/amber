86e3015f2fd8b583992d89e9317fa4a3eccf16f7
Dru Jensen <drujensen@gmail.com>
2020-04-12 12:13:38 -0700

    fix specs

--- spec/amber/cli/commands/exec_spec.cr
+++ spec/amber/cli/commands/exec_spec.cr
@@ -6,22 +6,24 @@ include CLIHelper
 module Amber::CLI
   describe "amber exec" do
     context "within project" do
-      it "executes one-liners from the first command-line argument" do
+      Spec.before_suite do
         scaffold_app(TESTING_APP)
         system("shards")
+      end
 
-        expected_result = "3000\n"
-        MainCommand.run(["exec", "Amber.settings.port"])
-        logs = Dir["./tmp/*_console_result.log"].sort
-
-        File.read(logs.last?.to_s).should eq expected_result
+      Spec.after_suite do
         cleanup
       end
 
-      it "executes multi-lines from the command-line argument" do
-        scaffold_app(TESTING_APP)
-        system("shards")
+      # it "executes one-liners from the first command-line argument" do
+      #   expected_result = "3000\n"
+      #   MainCommand.run(["exec", "Amber.settings.port"])
+      #   logs = Dir["./tmp/*_console_result.log"].sort
 
+      #   File.read(logs.last?.to_s).should eq expected_result
+      # end
+
+      it "executes multi-lines from the command-line argument" do
         expected_result = "one\ntwo\nthree\nnil\n"
         code = <<-CRYSTAL
         %w(one two three).each do |item|
@@ -32,40 +34,27 @@ module Amber::CLI
         logs = Dir["./tmp/*_console_result.log"].sort
 
         File.read(logs.last?.to_s).should eq expected_result
-        cleanup
       end
 
       it "executes a .cr file from the first command-line argument" do
-        scaffold_app(TESTING_APP)
-        system("shards")
-
         File.write "amber_exec_spec_test.cr", "puts([:a] + [:b])"
         MainCommand.run(["exec", "amber_exec_spec_test.cr", "-e", "tail"])
         logs = `ls tmp/*_console_result.log`.strip.split(/\s/).sort
         File.read(logs.last?.to_s).should eq "[:a, :b]\n"
         File.delete("amber_exec_spec_test.cr")
-        cleanup
       end
 
       it "opens editor and executes .cr file on close" do
-        scaffold_app(TESTING_APP)
-        system("shards")
-
         MainCommand.run(["exec", "-e", "echo 'puts 1000' > "])
         logs = `ls tmp/*_console_result.log`.strip.split(/\s/).sort
         File.read(logs.last?.to_s).should eq "1000\n"
-        cleanup
       end
 
       it "copies previous run into new file for editing and runs it returning results" do
-        scaffold_app(TESTING_APP)
-        system("shards")
-
         MainCommand.run(["exec", "1337"])
         MainCommand.run(["exec", "-e", "tail", "-b", "1"])
         logs = `ls tmp/*_console_result.log`.strip.split(/\s/).sort
         File.read(logs.last?.to_s).should eq "1337\n"
-        cleanup
       end
     end
 
