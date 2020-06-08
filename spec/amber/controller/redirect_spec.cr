c534825068e11c97034cb7c7acce695757a0b9d1
Opak Alex <opak.alexandr@gmail.com>
2020-06-08 18:31:04 +0200

    fix style

--- spec/amber/controller/redirect_spec.cr
+++ spec/amber/controller/redirect_spec.cr
@@ -64,7 +64,7 @@ module Amber::Controller::Helpers
       end
 
       it "redirects to full controller name as symbol" do
-	# tmp fix for current travis crystal version.
+        # tmp fix for current travis crystal version.
         Amber::Server.router.draw :web do
           get "/redirect/:id", RedirectController, :show
           get "/redirect/:id/edit", RedirectController, :edit
