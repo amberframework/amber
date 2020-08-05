589a5ce9ea2295ba59d62255b3f5a108f164e8fe
Elias J. Perez <eliasjpr@gmail.com>
2020-08-05 10:26:08 -0400

    [CORS]  Process Requests As Normal for Non CORS
    Issue: https://github.com/amberframework/amber/issues/1224
    
    Why?
    
    When using CORS pipe all requests are being validated as CORS.
    
    If validation fails 403 forbidden is thrown
    
    Changes
    
    - If origin header is not present process the request as a normal one
    
    Side Effects
    
    - Non CORS  requests are processed requests as normal when CORS handler is present
    
    Removes extra line
    
    move method back
    
    Missing context
    
    Return for non CORS requests

--- spec/support/helpers/router_helper.cr
+++ spec/support/helpers/router_helper.cr
@@ -60,6 +60,12 @@ module RouterHelper
     origin_header.should_not be_nil
   end
 
+  def assert_non_cors_request(context)
+    origin_header = context.response.headers["Access-Control-Allow-Origin"]?
+    context.response.status_code.should eq 200
+    origin_header.should be_nil
+  end
+
   def assert_cors_failure(context)
     origin_header = context.response.headers["Access-Control-Allow-Origin"]?
     context.response.status_code.should eq 403
