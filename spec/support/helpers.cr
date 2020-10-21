9628bb4034b32e3cbcf6b1756917b88d14778d4e
Andrew <49383564+andrewc910@users.noreply.github.com>
2020-10-21 11:17:45 -0400

    Plugin: Add support for a config file (#1235)
    * Add support for config file when installing plugins
    
    * Add spec for plugin autogenerated routes

--- spec/support/helpers.cr
+++ spec/support/helpers.cr
@@ -1,5 +1,6 @@
 require "./helpers/controller_helper"
 require "./helpers/cookie_helper"
+require "./helpers/plugin_helper"
 require "./helpers/session_helper"
 require "./helpers/router_helper"
 require "./helpers/websockets_helper"
@@ -8,6 +9,7 @@ require "./helpers/validations_helper"
 module Helpers
   include ControllerHelper
   include CookieHelper
+  include PluginHelper
   include RouterHelper
   include WebsocketsHelper
   include ValidationsHelper
