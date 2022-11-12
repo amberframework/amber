d2e2944d2876e6246962ca5db28221abb7c41aa9
Dru Jensen <drujensen@gmail.com>
2022-11-12 16:38:52 +0000

    refactor: cleanup more tests

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ FROM crystallang/crystal:latest
 ARG DEBIAN_FRONTEND=noninteractive
 RUN apt-get update -qq && apt-get install -y --no-install-recommends libpq-dev libsqlite3-dev libmysqlclient-dev libreadline-dev git curl vim netcat
 
+# Install Node
+RUN curl -sL https://deb.nodesource.com/setup_14.x | bash -
+RUN apt-get install -y nodejs
+
 WORKDIR /opt/amber
 
 # Build Amber
--- spec/amber/cli/commands/database_spec.cr
+++ spec/amber/cli/commands/database_spec.cr
@@ -7,51 +7,50 @@ include CLIFixtures
 
 module Amber::CLI
   describe "database" do
-    ENV["AMBER_ENV"] = "test"
+    Spec.around_each do |test|
+      # override DATABASE_URL for this test suite
+      if ENV["DATABASE_URL"]?
+        db_url = ENV["DATABASE_URL"]
+        ENV.delete("DATABASE_URL")
+        test.run
+        ENV["DATABASE_URL"] = db_url
+      else
+        test.run
+      end
+    end
 
     describe "sqlite" do
-      context ENV["AMBER_ENV"] do
-        it "has connection settings in config/environments/env.yml" do
-          env_yml = prepare_test_app
-          env_yml["database_url"].should eq expected_db_url("sqlite3", env)
-          cleanup
-        end
+      it "has connection settings in config/environments/env.yml" do
+        env_yml = prepare_test_app
+        env_yml["database_url"].should eq expected_db_url("sqlite3", env)
+        cleanup
+      end
 
-        it "does not create the database when db create" do
-          env_yml = prepare_test_app
-          db_filename = env_yml["database_url"].to_s.gsub("sqlite3:", "")
-          File.exists?(db_filename).should be_false
-          cleanup
-        end
+      it "creates and deletes the database when db migrate and drop" do
+        env_yml = prepare_test_app
+        CLI.settings.database_url = env_yml["database_url"].to_s
 
-        it "creates and deletes the database when db migrate and drop" do
-          env_yml = prepare_test_app
-          CLI.settings.database_url = env_yml["database_url"].to_s
+        MainCommand.run ["generate", "model", "-y", "Post"]
+        MainCommand.run ["db", "migrate"]
 
-          MainCommand.run ["generate", "model", "-y", "Post"]
-          MainCommand.run ["db", "migrate"]
+        db_filename = CLI.settings.database_url.to_s.gsub("sqlite3:", "")
+        File.exists?(db_filename).should be_true
+        File.info(db_filename).size.should_not eq 0
 
-          db_filename = CLI.settings.database_url.to_s.gsub("sqlite3:", "")
-          File.exists?(db_filename).should be_true
-          File.info(db_filename).size.should_not eq 0
+        MainCommand.run ["db", "drop"]
 
-          MainCommand.run ["db", "drop"]
-
-          db_filename = CLI.settings.database_url.gsub("sqlite3:", "")
-          File.exists?(db_filename).should be_false
-          cleanup
-        end
+        db_filename = CLI.settings.database_url.gsub("sqlite3:", "")
+        File.exists?(db_filename).should be_false
+        cleanup
       end
     end
 
     describe "postgres" do
-      context ENV["AMBER_ENV"] do
-        it "has #{ENV["AMBER_ENV"]}  connection settings" do
-          scaffold_app("#{TESTING_APP}", "-d", "pg")
-          env_yml = environment_yml(ENV["AMBER_ENV"], "#{Dir.current}/config/environments/")
-          env_yml["database_url"].should eq expected_db_url("pg", env)
-          cleanup
-        end
+      it "has test connection settings" do
+        scaffold_app("#{TESTING_APP}", "-d", "pg")
+        env_yml = environment_yml("test", "#{Dir.current}/config/environments/")
+        env_yml["database_url"].should eq expected_db_url("pg", env)
+        cleanup
       end
     end
   end
